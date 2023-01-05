1852f76a438833b2083d8c044f3d828c7071f534
Alvaro Prieto <alvaro.prieto@sofarocean.com>
2023-01-05 13:49:55 -0800

    Update baud rate to 921600

--- src/bsp/nucleo_u575/Core/Src/usart.c
+++ src/bsp/nucleo_u575/Core/Src/usart.c
@@ -71,7 +71,7 @@ void MX_USART1_UART_Init(void)
 
   /* USER CODE END USART1_Init 1 */
   USART_InitStruct.PrescalerValue = LL_USART_PRESCALER_DIV1;
-  USART_InitStruct.BaudRate = 115200;
+  USART_InitStruct.BaudRate = 921600;
   USART_InitStruct.DataWidth = LL_USART_DATAWIDTH_8B;
   USART_InitStruct.StopBits = LL_USART_STOPBITS_1;
   USART_InitStruct.Parity = LL_USART_PARITY_NONE;
