e1a1f7378cc3674f849c73074b637dd9a05f598c
Alvaro Prieto <alvaro.prieto@sofarocean.com>
2023-01-25 14:15:14 -0800

    Fix adin reset
    * Changed ADIN_RST to be open drain instead of push-pull
    * Fixed adi_bsp_hw_reset to actually reset the device 😂
    * Call adi_bsp_hw_reset in adi_bsp_init()
    * Reduced thread stack sizes a bit

--- src/bsp/nucleo_u575/Core/Src/gpio.c
+++ src/bsp/nucleo_u575/Core/Src/gpio.c
@@ -113,7 +113,7 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Pin = ADIN_RST_Pin;
   GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
   GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
-  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_OPENDRAIN;
   GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
   LL_GPIO_Init(ADIN_RST_GPIO_Port, &GPIO_InitStruct);
 
