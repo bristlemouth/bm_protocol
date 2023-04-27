9fae00d89239e6dd2052818442628a563ad8a9ea
Victor Sowa <victor.sowa@sofarocean.com>
2023-04-27 15:51:02 -0700

    oh wait here it is actually working lol

--- src/lib/bcmp/bcmp_ping.cpp
+++ src/lib/bcmp/bcmp_ping.cpp
@@ -10,28 +10,31 @@
 
 static uint64_t _ping_request_time;
 static uint32_t _bcmp_seq;
+static uint8_t* _expected_payload;
 
 err_t bcmp_send_ping_request(uint64_t node_id, const ip_addr_t *addr, uint8_t* payload, uint16_t payload_len) {
 
   uint16_t echo_len = sizeof(bcmp_echo_request_t) + payload_len;
 
-  uint8_t *echo_req_buff = (uint8_t *)pvPortMalloc(echo_len);
+  uint8_t *echo_req_buff = static_cast<uint8_t *>(pvPortMalloc(echo_len));
   configASSERT(echo_req_buff);
 
   memset(echo_req_buff, 0, echo_len);
 
-  bcmp_echo_request_t *echo_req = (bcmp_echo_request_t *) echo_req_buff;
+  bcmp_echo_request_t *echo_req = static_cast<bcmp_echo_request_t *>(echo_req_buff);
 
   echo_req->target_node_id = node_id;
-  echo_req->id = 0; // TODO - make this a randomly generated number
+  echo_req->id = (uint16_t)getNodeId(); // TODO - make this a randomly generated number
   echo_req->seq_num = _bcmp_seq++;
   echo_req->payload_len = payload_len;
 
-  memcpy(&echo_req->payload[0], payload, payload_len);
+  if (payload != NULL) {
+    memcpy(&echo_req->payload[0], payload, payload_len);
+  }
 
   printf("PING (%" PRIx64 "): %" PRIu16 " data bytes\n", echo_req->target_node_id, echo_req->payload_len);
 
-  err_t rval = bcmp_tx(addr, BCMP_ECHO_REQUEST, (uint8_t*)echo_req, sizeof(*echo_req));
+  err_t rval = bcmp_tx(addr, BCMP_ECHO_REQUEST, static_cast<uint8_t*>(echo_req), sizeof(*echo_req));
 
   _ping_request_time = uptimeGetMicroSeconds();
 
@@ -42,7 +45,7 @@ err_t bcmp_send_ping_request(uint64_t node_id, const ip_addr_t *addr, uint8_t* p
 
 err_t bcmp_send_ping_reply(bcmp_echo_reply_t *echo_reply, const ip_addr_t *addr) {
 
-  return bcmp_tx(addr, BCMP_ECHO_REPLY, (uint8_t*)echo_reply, sizeof(*echo_reply));
+  return bcmp_tx(addr, BCMP_ECHO_REPLY, static_cast<uint8_t*>(echo_reply), sizeof(*echo_reply));
 }
 
 err_t bcmp_process_ping_request(bcmp_echo_request_t *echo_req, const ip_addr_t *src, const ip_addr_t *dst) {
@@ -52,7 +55,7 @@ err_t bcmp_process_ping_request(bcmp_echo_request_t *echo_req, const ip_addr_t *
   configASSERT(echo_req);
   if ((echo_req->target_node_id == 0) || (getNodeId() == echo_req->target_node_id)) {
     echo_req->target_node_id = getNodeId();
-    return bcmp_send_ping_reply((bcmp_echo_reply_t *)echo_req, dst);
+    return bcmp_send_ping_reply(static_cast<bcmp_echo_reply_t*>(echo_req), dst);
   }
 
   return ERR_OK;
@@ -60,10 +63,12 @@ err_t bcmp_process_ping_request(bcmp_echo_request_t *echo_req, const ip_addr_t *
 
 err_t bcmp_process_ping_reply(bcmp_echo_reply_t *echo_reply){
   configASSERT(echo_reply);
-
-  // todo capture a time stamp for when the message was recieved
-  uint64_t diff = uptimeGetMicroSeconds() - _ping_request_time;
-  printf("🏓 %" PRIu16 " bytes from %" PRIx64 " bcmp_seq=%" PRIu32 " time=%" PRIu64 " us\n", echo_reply->payload_len, echo_reply->node_id, echo_reply->seq_num, diff);
+  // TODO - once we have random numbers working we can then use a static
+  //        number to check
+  if((uint16_t)getNodeId() == echo_reply->id){
+    uint64_t diff = uptimeGetMicroSeconds() - _ping_request_time;
+    printf("🏓 %" PRIu16 " bytes from %" PRIx64 " bcmp_seq=%" PRIu32 " time=%" PRIu64 " ms\n", echo_reply->payload_len, echo_reply->node_id, echo_reply->seq_num, diff/1000);
+  }
 
   return ERR_OK;
 }
