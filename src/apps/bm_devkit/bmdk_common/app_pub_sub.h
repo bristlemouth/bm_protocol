6027e64145cd5a6fdeca0275f9da09562d9197dc
Tim Johnson <2293335+tcj@users.noreply.github.com>
2023-09-06 14:10:45 -0700

    Add a defined value for targeting all nodes, instead of using magic number zero (#462)

--- src/apps/bm_devkit/bmdk_common/app_pub_sub.h
+++ src/apps/bm_devkit/bmdk_common/app_pub_sub.h
@@ -11,6 +11,9 @@ extern "C" {
 #define APP_PUB_SUB_UTC_TYPE            1
 #define APP_PUB_SUB_UTC_VERSION         1
 
+// A target_node_id of 0 means all targets
+#define BM_ALL_NODES                    0
+
 #ifdef __cplusplus
 }
 #endif
