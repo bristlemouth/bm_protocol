59fd39a5afb3087ec4369942b08c6766ffed6a4d
Russell DeGuzman <5218083+russelldeguzman@users.noreply.github.com>
2023-10-30 12:56:39 -0700

    Sc 196416/config map service (#24)
    * new messages
    
    adding the service
    
    init services
    
    debug callback
    
    updating node id
    
    fix encoding
    
    new stuff
    
    fix cfg
    
    ../../src/lib/sys/configuration.cpp
    
    handle failure properly
    
    * bump bm_common_messages
    
    * pr feedback

--- src/apps/bm_devkit/bmdk_common/app_main.cpp
+++ src/apps/bm_devkit/bmdk_common/app_main.cpp
@@ -55,6 +55,7 @@
 #include "debug_bm_service.h"
 #include "echo_service.h"
 #include "sys_info_service.h"
+#include "config_cbor_map_service.h"
 
 /* USER FILE INCLUDES */
 #include "user_code.h"
@@ -399,7 +400,8 @@ static void defaultTask(void *parameters) {
   bm_sub(APP_PUB_SUB_UTC_TOPIC, handle_bm_subscriptions);
   echo_service_init();
   sys_info_service_init(debug_configuration_system);
-
+  config_cbor_map_service_init(debug_configuration_hardware, debug_configuration_system,
+                               debug_configuration_user);
 #ifdef USE_MICROPYTHON
   micropython_freertos_init(&usbCLI);
 #endif
