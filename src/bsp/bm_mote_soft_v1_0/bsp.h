8ecb20ab8ebaf915c4ffd669c15106040ee87a45
Victor Sowa <victor.sowa@sofarocean.com>
2023-11-14 13:23:41 -0800

    make a copy of the bristleback bsp to be modified for the TSYS (SOFT) bsp

---
+++ src/bsp/bm_mote_soft_v1_0/bsp.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#define BSP_NAME "mote_bristleback_v1_0"
+
+#include "main.h"
+#include "stm32u5xx.h"
+#include "protected_spi.h"
+#include "protected_i2c.h"
+#include "io.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define DEBUG_USE_LPUART1
+#define DEBUG_USE_USART3
+
+#define STM32_UUID ((uint32_t *)0x1FFF7A10)
+
+// Bristleback LED logic levels.
+#define BB_LED_ON (1)
+#define BB_LED_OFF (0)
+
+void mxInit(void);
+void bspInit();
+
+// Pin definitions
+extern IOPinHandle_t LED_GREEN;
+extern IOPinHandle_t GPIO1;
+extern IOPinHandle_t VUSB_DETECT;
+extern IOPinHandle_t BB_3V3_EN;
+extern IOPinHandle_t LED_RED;
+extern IOPinHandle_t BB_PL_BUCK_EN;
+extern IOPinHandle_t BM_SCK_RX3;
+extern IOPinHandle_t BM_MISO;
+extern IOPinHandle_t BM_MOSI_TX3;
+extern IOPinHandle_t LED_BLUE;
+extern IOPinHandle_t BB_VBUS_EN;
+extern IOPinHandle_t FLASH_SCK;
+extern IOPinHandle_t FLASH_MISO;
+extern IOPinHandle_t FLASH_MOSI;
+extern IOPinHandle_t FLASH_CS;
+extern IOPinHandle_t ADIN_CS;
+extern IOPinHandle_t ADIN_PWR;
+extern IOPinHandle_t ADIN_RST;
+extern IOPinHandle_t ADIN_SCK;
+extern IOPinHandle_t ADIN_MISO;
+extern IOPinHandle_t ADIN_MOSI;
+extern IOPinHandle_t BOOT_LED;
+extern IOPinHandle_t ADIN_INT;
+extern IOPinHandle_t DEBUG_RX;
+extern IOPinHandle_t DEBUG_TX;
+extern IOPinHandle_t PAYLOAD_RX;
+extern IOPinHandle_t PAYLOAD_TX;
+
+// SPI Interfaces
+extern SPIInterface_t spi1;
+extern SPIInterface_t spi2;
+extern SPIInterface_t spi3;
+
+// I2C Interfaces
+extern I2CInterface_t i2c1;
+
+bool usb_is_connected();
+
+typedef struct adin_pins_s {
+	SPIInterface_t *spiInterface;
+	IOPinHandle_t *chipSelect;
+	IOPinHandle_t *interrupt;
+	IOPinHandle_t *reset;
+} adin_pins_t;
+
+#define I2C_INA_MAIN_ADDR  (0x43)
+#define I2C_INA_PODL_ADDR  (0x41)
+#define NUM_INA232_DEV (2)
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/bsp/bm_mote_soft_v1_0/bsp_pins.c
@@ -0,0 +1,29 @@
+#include "bsp.h"
+#include "stm32_io.h"
+#include "pca9535.h"
+
+IOPinHandle_t LED_GREEN = {&STM32PinDriver, &(STM32Pin_t){LED_GREEN_GPIO_Port, LED_GREEN_Pin, NULL, NULL}};
+IOPinHandle_t GPIO1 = {&STM32PinDriver, &(STM32Pin_t){GPIO1_GPIO_Port, GPIO1_Pin, NULL, NULL}};
+IOPinHandle_t VUSB_DETECT = {&STM32PinDriver, &(STM32Pin_t){VUSB_DETECT_GPIO_Port, VUSB_DETECT_Pin, NULL, NULL}};
+IOPinHandle_t BB_3V3_EN = {&STM32PinDriver, &(STM32Pin_t){BB_3V3_EN_GPIO_Port, BB_3V3_EN_Pin, NULL, NULL}};
+IOPinHandle_t LED_RED = {&STM32PinDriver, &(STM32Pin_t){LED_RED_GPIO_Port, LED_RED_Pin, NULL, NULL}};
+IOPinHandle_t BB_PL_BUCK_EN = {&STM32PinDriver, &(STM32Pin_t){BB_PL_BUCK_EN_GPIO_Port, BB_PL_BUCK_EN_Pin, NULL, NULL}};
+IOPinHandle_t BM_SCK_RX3 = {&STM32PinDriver, &(STM32Pin_t){BM_SCK_RX3_GPIO_Port, BM_SCK_RX3_Pin, NULL, NULL}};
+IOPinHandle_t BM_MISO = {&STM32PinDriver, &(STM32Pin_t){BM_MISO_GPIO_Port, BM_MISO_Pin, NULL, NULL}};
+IOPinHandle_t BM_MOSI_TX3 = {&STM32PinDriver, &(STM32Pin_t){BM_MOSI_TX3_GPIO_Port, BM_MOSI_TX3_Pin, NULL, NULL}};
+IOPinHandle_t LED_BLUE = {&STM32PinDriver, &(STM32Pin_t){LED_BLUE_GPIO_Port, LED_BLUE_Pin, NULL, NULL}};
+IOPinHandle_t BB_VBUS_EN = {&STM32PinDriver, &(STM32Pin_t){BB_VBUS_EN_GPIO_Port, BB_VBUS_EN_Pin, NULL, NULL}};
+IOPinHandle_t FLASH_SCK = {&STM32PinDriver, &(STM32Pin_t){FLASH_SCK_GPIO_Port, FLASH_SCK_Pin, NULL, NULL}};
+IOPinHandle_t FLASH_MISO = {&STM32PinDriver, &(STM32Pin_t){FLASH_MISO_GPIO_Port, FLASH_MISO_Pin, NULL, NULL}};
+IOPinHandle_t FLASH_MOSI = {&STM32PinDriver, &(STM32Pin_t){FLASH_MOSI_GPIO_Port, FLASH_MOSI_Pin, NULL, NULL}};
+IOPinHandle_t FLASH_CS= {&STM32PinDriver, &(STM32Pin_t){FLASH_CS_GPIO_Port, FLASH_CS_Pin, NULL, NULL}};
+IOPinHandle_t ADIN_PWR = {&STM32PinDriver, &(STM32Pin_t){ADIN_PWR_GPIO_Port, ADIN_PWR_Pin, NULL, NULL}};
+IOPinHandle_t ADIN_RST = {&STM32PinDriver, &(STM32Pin_t){ADIN_RST_GPIO_Port, ADIN_RST_Pin, NULL, NULL}};
+IOPinHandle_t ADIN_CS = {&STM32PinDriver, &(STM32Pin_t){ADIN_CS_GPIO_Port, ADIN_CS_Pin, NULL, NULL}};
+IOPinHandle_t ADIN_SCK = {&STM32PinDriver, &(STM32Pin_t){ADIN_SCK_GPIO_Port, ADIN_SCK_Pin, NULL, NULL}};
+IOPinHandle_t ADIN_MISO = {&STM32PinDriver, &(STM32Pin_t){ADIN_MISO_GPIO_Port, ADIN_MISO_Pin, NULL, NULL}};
+IOPinHandle_t ADIN_MOSI = {&STM32PinDriver, &(STM32Pin_t){ADIN_MOSI_GPIO_Port, ADIN_MOSI_Pin, NULL, NULL}};
+IOPinHandle_t ADIN_INT = {&STM32PinDriver, &(STM32Pin_t){ADIN_INT_GPIO_Port, ADIN_INT_Pin, NULL, NULL}};
+IOPinHandle_t BOOT_LED = {&STM32PinDriver, &(STM32Pin_t){BOOT_LED_GPIO_Port, BOOT_LED_Pin, NULL, NULL}};
+IOPinHandle_t PAYLOAD_RX = {&STM32PinDriver, &(STM32Pin_t){PAYLOAD_RX_GPIO_Port, PAYLOAD_RX_Pin, NULL, NULL}};
+IOPinHandle_t PAYLOAD_TX = {&STM32PinDriver, &(STM32Pin_t){PAYLOAD_TX_GPIO_Port, PAYLOAD_TX_Pin, NULL, NULL}};
