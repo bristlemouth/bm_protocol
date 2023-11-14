b264d3da9a3e01d948c7560d872949e8b52cfea3
Victor Sowa <85895527+victorsowa12@users.noreply.github.com>
2023-11-14 12:13:54 -0800

    Sc 197055/min max agg checks (#40)
    * just using one min max for now.
    
    * rename to sample min/max
    
    * do min max for each different type of reading + add a #define for pi.
    
    * woops - make alligator mouths the right way
    
    * do calcs once, and SAMPLE_MEMBER
    
    * remove paranthesis

--- src/apps/bridge/aanderaaController.cpp
+++ src/apps/bridge/aanderaaController.cpp
@@ -70,6 +70,16 @@ static constexpr uint32_t SAMPLE_TIMER_MS = 30 * 1000;
 static constexpr uint32_t TOPO_TIMEOUT_MS = 10 * 1000;
 static constexpr uint32_t NODE_INFO_TIMEOUT_MS = 1000;
 
+#define PI  3.14159265358979323846
+
+static constexpr double DIRECTION_SAMPLE_MEMBER_MIN = 0.0;
+static constexpr double DIRECTION_SAMPLE_MEMBER_MAX = 2*PI;
+static constexpr double ABS_SPEED_SAMPLE_MEMBER_MIN = 0.0;
+static constexpr double ABS_SPEED_SAMPLE_MEMBER_MAX = 300.0;
+static constexpr double TEMP_SAMPLE_MEMBER_MIN = -5.0;
+static constexpr double TEMP_SAMPLE_MEMBER_MAX = 40.0;
+
+
 static void createAanderaaSub(uint64_t node_id);
 static void runController(void *param);
 static Aanderaa_t *findAanderaaById(uint64_t node_id);
@@ -241,11 +251,26 @@ static void runController(void *param) {
             // If not send NaNs for all the values.
             // TODO - verify that we can assume if one sampler is below the min then all of them are.
             if(curr->abs_speed_cm_s.getNumSamples() >= MIN_READINGS_FOR_AGGREGATION) {
-              agg.abs_speed_mean_cm_s = curr->abs_speed_cm_s.getMean(true);;
-              agg.abs_speed_std_cm_s = curr->abs_speed_cm_s.getStd(true);;
-              agg.direction_circ_mean_rad = curr->direction_rad.getCircularMean();;
+              agg.abs_speed_mean_cm_s = curr->abs_speed_cm_s.getMean(true);
+              agg.abs_speed_std_cm_s = curr->abs_speed_cm_s.getStd(true);
+              agg.direction_circ_mean_rad = curr->direction_rad.getCircularMean();
               agg.direction_circ_std_rad = curr->direction_rad.getCircularStd();
-              agg.temp_mean_deg_c = curr->temp_deg_c.getMean(true);;
+              agg.temp_mean_deg_c = curr->temp_deg_c.getMean(true);
+              if (agg.abs_speed_mean_cm_s < ABS_SPEED_SAMPLE_MEMBER_MIN || agg.abs_speed_mean_cm_s > ABS_SPEED_SAMPLE_MEMBER_MAX) {
+                agg.abs_speed_mean_cm_s = NAN;
+              }
+              if (agg.abs_speed_std_cm_s < ABS_SPEED_SAMPLE_MEMBER_MIN || agg.abs_speed_std_cm_s > ABS_SPEED_SAMPLE_MEMBER_MAX) {
+              agg.abs_speed_std_cm_s = NAN;
+              }
+              if (agg.direction_circ_mean_rad < DIRECTION_SAMPLE_MEMBER_MIN || agg.direction_circ_mean_rad > DIRECTION_SAMPLE_MEMBER_MAX) {
+                agg.direction_circ_mean_rad = NAN;
+              }
+              if (agg.direction_circ_std_rad < DIRECTION_SAMPLE_MEMBER_MIN || agg.direction_circ_std_rad > DIRECTION_SAMPLE_MEMBER_MAX) {
+                agg.direction_circ_std_rad = curr->direction_rad.getCircularStd();
+              }
+              if (agg.temp_mean_deg_c < TEMP_SAMPLE_MEMBER_MIN || agg.temp_mean_deg_c > TEMP_SAMPLE_MEMBER_MAX) {
+                agg.temp_mean_deg_c = NAN;
+              }
             }
             static constexpr uint8_t TIME_STR_BUFSIZE = 50;
             static char timeStrbuf[TIME_STR_BUFSIZE];
