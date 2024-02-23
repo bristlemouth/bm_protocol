80bc1466d6bcf9953b6f2638a1bc3cfacbd64aaa
Victor Sowa <85895527+victorsowa12@users.noreply.github.com>
2024-02-23 13:26:43 -0800

    increase the bridges freertos heap size to 400KB (#87)

--- src/apps/bridge/FreeRTOSConfig.h
+++ src/apps/bridge/FreeRTOSConfig.h
@@ -69,7 +69,7 @@ extern "C" {
 #define configTICK_RATE_HZ                       ((TickType_t)1000)
 #define configMAX_PRIORITIES                     ( 32 )
 #define configMINIMAL_STACK_SIZE                 ((uint16_t)128)
-#define configTOTAL_HEAP_SIZE                    ((size_t)1024*256)
+#define configTOTAL_HEAP_SIZE                    ((size_t)1024*400)
 #define configMAX_TASK_NAME_LEN                  ( 16 )
 #define configUSE_TRACE_FACILITY                 1
 #define configUSE_16_BIT_TICKS                   0
