38474bb4a216ea9b70cf778a51f1282078466e1b
Russell DeGuzman <5218083+russelldeguzman@users.noreply.github.com>
2024-02-28 13:25:47 -0800

    support logging of network configs (#92)
    * support logging of network configs
    
    * adding a crc tag
    
    * add a check before sending to print
    
    * add rtc time
    
    * add config list to log
    
    * cleanup and add crc list
    
    * cleanup
    
    * alloc instead of access
    
    * alloc test

--- src/apps/bridge/app_pub_sub.h
+++ src/apps/bridge/app_pub_sub.h
@@ -1,52 +1,55 @@
 #pragma once
 
-#include <stdint.h>
 #include "bm_common_pub_sub.h"
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define APP_PUB_SUB_UTC_TOPIC           "spotter/utc-time"
-#define APP_PUB_SUB_UTC_TYPE            1
-#define APP_PUB_SUB_UTC_VERSION         1
+#define APP_PUB_SUB_UTC_TOPIC "spotter/utc-time"
+#define APP_PUB_SUB_UTC_TYPE 1
+#define APP_PUB_SUB_UTC_VERSION 1
 
+#define APP_PUB_SUB_LAST_NET_CFG_TOPIC "spotter/request-last-network-config"
+#define APP_PUB_SUB_LAST_NET_CFG_TYPE 1
+#define APP_PUB_SUB_LAST_NET_CFG_VERSION 1
 
-#define APP_PUB_SUB_LAST_NET_CFG_TOPIC          "spotter/request-last-network-config"
-#define APP_PUB_SUB_LAST_NET_CFG_TYPE            1
-#define APP_PUB_SUB_LAST_NET_CFG_VERSION         1
+#define APP_PUB_SUB_PRINTF_TOPIC "printf"
+#define APP_PUB_SUB_PRINTF_TYPE 1
+#define APP_PUB_SUB_PRINTF_VERSION 1
 
-#define APP_PUB_SUB_PRINTF_TOPIC        "printf"
-#define APP_PUB_SUB_PRINTF_TYPE         1
-#define APP_PUB_SUB_PRINTF_VERSION      1
+#define APP_PUB_SUB_BUTTON_TOPIC "button"
+#define APP_PUB_SUB_BUTTON_TYPE 1
+#define APP_PUB_SUB_BUTTON_VERSION 1
+#define APP_PUB_SUB_BUTTON_CMD_ON "on"
+#define APP_PUB_SUB_BUTTON_CMD_OFF "off"
 
-#define APP_PUB_SUB_BUTTON_TOPIC        "button"
-#define APP_PUB_SUB_BUTTON_TYPE         1
-#define APP_PUB_SUB_BUTTON_VERSION      1
-#define APP_PUB_SUB_BUTTON_CMD_ON       "on"
-#define APP_PUB_SUB_BUTTON_CMD_OFF      "off"
+#define APP_PUB_SUB_BM_BRIDGE_PRINTF_TOPIC "bridge/printf"
+#define APP_PUB_SUB_BM_BRIDGE_PRINTF_TYPE 1
+#define APP_PUB_SUB_BM_BRIDGE_PRINTF_VERSION 1
 
-#define APP_PUB_SUB_BM_BRIDGE_PRINTF_TOPIC       "bridge/printf"
-#define APP_PUB_SUB_BM_BRIDGE_PRINTF_TYPE        1
-#define APP_PUB_SUB_BM_BRIDGE_PRINTF_VERSION     1
+#define APP_PUB_SUB_BM_BRIDGE_CFG_PRINTF_TOPIC "bridge/cfg_printf"
+#define APP_PUB_SUB_BM_BRIDGE_CFG_PRINTF_TYPE 1
+#define APP_PUB_SUB_BM_BRIDGE_CFG_PRINTF_VERSION 1
 
-#define APP_PUB_SUB_BM_BRIDGE_SENSOR_IND_TOPIC       "bridge/sensor_ind_log"
-#define APP_PUB_SUB_BM_BRIDGE_SENSOR_IND_TYPE        1
-#define APP_PUB_SUB_BM_BRIDGE_SENSOR_IND_VERSION     1
+#define APP_PUB_SUB_BM_BRIDGE_SENSOR_IND_TOPIC "bridge/sensor_ind_log"
+#define APP_PUB_SUB_BM_BRIDGE_SENSOR_IND_TYPE 1
+#define APP_PUB_SUB_BM_BRIDGE_SENSOR_IND_VERSION 1
 
-#define APP_PUB_SUB_BM_BRIDGE_SENSOR_AGG_TOPIC       "bridge/sensor_agg_log"
-#define APP_PUB_SUB_BM_BRIDGE_SENSOR_AGG_TYPE        1
-#define APP_PUB_SUB_BM_BRIDGE_SENSOR_AGG_VERSION     1
+#define APP_PUB_SUB_BM_BRIDGE_SENSOR_AGG_TOPIC "bridge/sensor_agg_log"
+#define APP_PUB_SUB_BM_BRIDGE_SENSOR_AGG_TYPE 1
+#define APP_PUB_SUB_BM_BRIDGE_SENSOR_AGG_VERSION 1
 
 typedef struct app_pub_sub_bm_bridge_sensor_report_data {
-    uint32_t bm_config_crc32;
-    size_t cbor_buffer_len;
-    uint8_t cbor_buffer[0];
+  uint32_t bm_config_crc32;
+  size_t cbor_buffer_len;
+  uint8_t cbor_buffer[0];
 } __attribute__((packed)) app_pub_sub_bm_bridge_sensor_report_data_t;
 
-#define APP_PUB_SUB_BM_BRIDGE_SENSOR_REPORT_TOPIC       "bridge/sensor_report"
-#define APP_PUB_SUB_BM_BRIDGE_SENSOR_REPORT_TYPE        1
-#define APP_PUB_SUB_BM_BRIDGE_SENSOR_REPORT_VERSION     1
+#define APP_PUB_SUB_BM_BRIDGE_SENSOR_REPORT_TOPIC "bridge/sensor_report"
+#define APP_PUB_SUB_BM_BRIDGE_SENSOR_REPORT_TYPE 1
+#define APP_PUB_SUB_BM_BRIDGE_SENSOR_REPORT_VERSION 1
 
 #ifdef __cplusplus
 }
