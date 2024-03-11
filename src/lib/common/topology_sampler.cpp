8945443b263ac413c835b18387afad497957aa6c
Victor Sowa <victor.sowa@sofarocean.com>
2024-03-11 15:12:08 -0700

    move the notifications out of the do-while such that they always fire!

--- src/lib/common/topology_sampler.cpp
+++ src/lib/common/topology_sampler.cpp
@@ -214,14 +214,13 @@ static void topology_sample_cb(networkTopology_t *networkTopology) {
         _send_on_boot = false;
       }
     }
-
-    // Should these two be outside of the do-while loop? That way we will always notify the other tasks?
-    // The first four inputs are not used by this message type
-    reportBuilderAddToQueue(0, 0, NULL, 0, REPORT_BUILDER_CHECK_CRC);
-    xTaskNotify(sensor_controller_task_handle, SAMPLER_TIMER_BITS, eSetBits);
-
   } while (0);
   xSemaphoreGive(_node_list.node_list_mutex);
+
+  // The first four inputs are not used by this message type
+  reportBuilderAddToQueue(0, 0, NULL, 0, REPORT_BUILDER_CHECK_CRC);
+  // Notify the sensor controller task to sample for sensors
+  xTaskNotify(sensor_controller_task_handle, SAMPLER_TIMER_BITS, eSetBits);
 
   if (cbor_buffer) {
     vPortFree(cbor_buffer);
