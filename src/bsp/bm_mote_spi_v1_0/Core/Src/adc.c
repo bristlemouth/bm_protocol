7b7455b36ab51c1cbc47557500b9b451e850d9d0
Victor Sowa <victor.sowa@sofarocean.com>
2024-03-12 17:54:14 -0700

    disable continuous mode

--- src/bsp/bm_mote_spi_v1_0/Core/Src/adc.c
+++ src/bsp/bm_mote_spi_v1_0/Core/Src/adc.c
@@ -49,7 +49,7 @@ void MX_ADC1_Init(void)
   hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
   hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
   hadc1.Init.LowPowerAutoWait = DISABLE;
-  hadc1.Init.ContinuousConvMode = ENABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
   hadc1.Init.NbrOfConversion = 1;
   hadc1.Init.DiscontinuousConvMode = DISABLE;
   hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
