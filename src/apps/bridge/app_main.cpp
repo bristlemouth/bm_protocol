0e01d01d333d8ee9d36268c0369f01ace072c5f0
Victor Sowa <victor.sowa@sofarocean.com>
2024-03-28 15:52:05 -0700

    woops only turn off the power controller when the bridge does have rtc set

--- src/apps/bridge/app_main.cpp
+++ src/apps/bridge/app_main.cpp
@@ -272,7 +272,7 @@ static void handle_subscriptions(uint64_t node_id, const char *topic, uint16_t t
   } else if (strncmp(APP_PUB_SUB_RTC_ZERO_TOPIC, topic, topic_len) == 0) {
     if (type == APP_PUB_SUB_RTC_ZERO_TYPE && version == APP_PUB_SUB_RTC_ZERO_VERSION) {
       // turn off the power controller!
-      if (!isRTCSet()) {
+      if (isRTCSet()) {
         bridgeLogPrint(BRIDGE_SYS, BM_COMMON_LOG_LEVEL_INFO, USE_HEADER,
                   "RTC 0 detected, turning off power controller!\n");
         _main_bridge_power_controller->powerControlEnable(false);
