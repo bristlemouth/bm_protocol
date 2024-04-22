9507f476c18f2aa8b94c322bbde54956e14f16a9
Victor Sowa <victor.sowa@sofarocean.com>
2024-04-22 16:11:21 -0700

    completely re-write the app and create a turbidity class to handle most of the functionality and leave the setup/loop code clean

---
+++ src/apps/bristleback_apps/seapoint_turbidity/user_code/seapoint_turbidity_sensor.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <stdint.h>
+#include "OrderedSeparatorLineParser.h"
+#include "bm_turbidity_data_msg.h"
+
+class SeapointTurbiditySensor {
+  public:
+    SeapointTurbiditySensor()
+        : _parser(",", 256, PARSER_VALUE_TYPE, 2){};
+    void init(); // TODO - needs scoping if needed
+    bool getData(BmTurbidityDataMsg::Data &d);
+    void flush(void); // TODO - needs scoping if needed
+
+  public:
+    static constexpr char SEAPOINT_TURBIDITY_RAW_LOG[] = "turbidity_raw.log";
+
+  private:
+    static constexpr uint32_t BAUD_RATE = 115200;
+    static constexpr char LINE_TERM = '\n';
+    static constexpr ValueType PARSER_VALUE_TYPE[] = {TYPE_DOUBLE, TYPE_DOUBLE};
+    static constexpr char SENSOR_BM_LOG_ENABLE[] = "sensorBmLogEnable";
+
+  private:
+    uint32_t _sensorBmLogEnable = 0;
+    OrderedSeparatorLineParser _parser;
+    char _payload_buffer[2048];
+};
