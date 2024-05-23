4f29c7a4d568004690e5a4a24f5b04dc62784168
Russell DeGuzman <5218083+russelldeguzman@users.noreply.github.com>
2024-05-23 10:38:16 -0700

    Bugfix/fixing difference signal (#138)
    * meanmake -j
    
    * signal mean
    
    * updating difference signal
    
    * adding descriptions to functions
    
    * smaller bbuf support
    
    * adding some comments

--- src/lib/common/differenceSignal.cpp
+++ src/lib/common/differenceSignal.cpp
@@ -1,6 +1,10 @@
 #include "differenceSignal.h"
 #include "FreeRTOS.h"
 
+/*!
+ * @brief Constructor for DifferenceSignal class
+ * @param numTotalSamples[in] Number of total samples of the signal to be stored
+ */
 DifferenceSignal::DifferenceSignal(uint32_t numTotalSamples) : r_i(0) {
   configASSERT(numTotalSamples > 0);
   r_n = numTotalSamples;
@@ -8,6 +12,11 @@ DifferenceSignal::DifferenceSignal(uint32_t numTotalSamples) : r_i(0) {
   configASSERT(r);
 }
 
+/*!
+ * @brief Adds a sample to the signal
+ * @param sample[in] The sample to be added
+ * @return True if the sample was added, false if the buffer is full
+ */
 bool DifferenceSignal::addSample(double sample) {
   if (r_i < r_n) {
     r[r_i++] = sample;
@@ -16,22 +25,26 @@ bool DifferenceSignal::addSample(double sample) {
   return false;
 }
 
+/*!
+ * @brief Encodes the difference signal to a buffer
+ * @param d_n[out] The buffer to store the difference signal
+ * @param numSamples[in, out] In: The buffer sample size, Out: The number of samples encoded into the buffer
+ * @return True if the difference signal was encoded, false otherwise
+ * @note Caller must ensure that the buffer is large enough to store the requested samples
+ */
 bool DifferenceSignal::encodeDifferenceSignalToBuffer(double *d_n, size_t &numSamples) {
   configASSERT(d_n);
   configASSERT(numSamples > 0);
   bool rval = false;
   do {
-    if (numSamples < (r_i)) {
-      break;
-    }
     if (!r_i) {
       break;
     }
-    d_n[0] = 0.0;
-    for (uint32_t i = 1; i < r_i; i++) {
-      d_n[i] = r[i] - r[i - 1];
+    size_t dn_len = r_i - 1;
+    numSamples = (numSamples < dn_len) ? numSamples : dn_len;
+    for (uint32_t i = 1; i < numSamples+1; i++) {
+      d_n[i-1] = r[i] - r[i - 1];
     }
-    numSamples = r_i;
     rval = true;
   } while (0);
   if (!rval) {
@@ -40,6 +53,41 @@ bool DifferenceSignal::encodeDifferenceSignalToBuffer(double *d_n, size_t &numSa
   return rval;
 }
 
+/*!
+ * @brief Clears the signal buffer
+ */
 void DifferenceSignal::clear() { r_i = 0; }
 
+/*!
+ * @brief Checks if the signal buffer is full
+ * @return True if the buffer is full, false otherwise
+ */
 bool DifferenceSignal::isFull() { return r_i == r_n; }
+
+/*!
+ * @brief Calculates the mean of the signal
+ * @return The mean of the signal
+ */
+double DifferenceSignal::signalMean() {
+    if(!r_i) {
+        return 0.0;
+    }
+    double sum = 0.0;
+    for (uint32_t i = 0; i < r_i; i++) {
+        sum += r[i];
+    }
+    return sum / r_i;
+}
+
+/*!
+ * @brief Gets the reference signal
+ * @param r0[out] The reference signal
+ * @return True if the reference signal was retrieved, false otherwise
+ */
+bool DifferenceSignal::getReferenceSignal(double &r0) {
+    if (!r_i) {
+        return false;
+    }
+    r0 = r[0];
+    return true;
+}
