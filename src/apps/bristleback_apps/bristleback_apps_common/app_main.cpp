5a8b84bdfbe7b16ddbc05858e15318a35019872e
Victor Sowa <victor.sowa@sofarocean.com>
2024-06-04 16:03:51 -0700

    clear the trace buffer after copying it over

--- src/apps/bristleback_apps/bristleback_apps_common/app_main.cpp
+++ src/apps/bristleback_apps/bristleback_apps_common/app_main.cpp
@@ -155,6 +155,7 @@ extern "C" int main(void) {
   // enterBootloaderIfNeeded();
 
   memcpy(user_traceEvents, traceEvents, sizeof(traceEvents));
+  memset(traceEvents, 0, sizeof(traceEvents));
   user_fullTicksLeft = fullTicksLeft;
   user_expectedTicks = expectedTicks;
   user_lpuart_start_cpu_cycles = lpuart_start;
