4b542c13af347695094277f33ff93032d82e071f
Victor Sowa <85895527+victorsowa12@users.noreply.github.com>
2024-09-19 15:36:27 -0700

    Feat/rs232 bsp (#162)
    * Create BSP for RS232 expander board
    
    Mostly a copy of bristleback with a few changes.
    Started basic test app.
    
    * copy/modify the bristleback_apps_common files to create test app
    
    * remove the unused pins/peripherals that are present on bristleback, but not on the rs232 board
    
    * a few more unused usart3 stuff. One big thing to note is that we need to make to always include the autogenerated cubemx usart hal files since serial.c expects them!
    
    * remove usart3 form app_main
    
    * remove reference to io expander that is not needed
    
    * add comment to main.h on why we need to include ll_usart.h
    
    * rename to vbus_en to remove the bb_ (meaning bristleback)
    
    * make sure the discharge_on pin is low by default
    
    * remove the comment
    
    * final spot where the pins needed to be renamed and make a new rs232_example_apps/rs232_example_apps_common and serial_payload_example
    
    * add serial_payload on rs232 to test build
    
    * remove the rs232 test app and since we can just use the serial payload example for testing
    
    * remove a couple more includes that are not needed here.
    
    * remove another unused include
    
    ---------
    
    Co-authored-by: Zachary Crockett <zachary.crockett@sofarocean.com>

---
+++ src/apps/rs232_expander_apps/serial_payload_example/user_code/user_code.cpp
@@ -0,0 +1,70 @@
+#include "user_code.h"
+#include "bm_network.h"
+#include "bm_printf.h"
+#include "bm_pubsub.h"
+#include "bsp.h"
+#include "debug.h"
+#include "lwip/inet.h"
+#include "payload_uart.h"
+#include "stm32_rtc.h"
+#include "task_priorities.h"
+#include "uptime.h"
+#include "usart.h"
+#include "util.h"
+
+#define LED_ON_TIME_MS 20
+#define LED_PERIOD_MS 1000
+
+bool led_state = false;
+
+// A buffer to put data from out payload sensor into.
+char payload_buffer[2048];
+
+void setup(void) {
+  /* USER ONE-TIME SETUP CODE GOES HERE */
+  // Setup the UART – the on-board serial driver that talks to the RS232 transceiver.
+  PLUART::init(USER_TASK_PRIORITY);
+  // Baud set per expected baud rate of the sensor.
+  PLUART::setBaud(9600);
+  // Set a line termination character per protocol of the sensor.
+  PLUART::setTerminationCharacter('\n');
+  // Turn on the UART.
+  PLUART::enable();
+  // Enable the input to the Vout power supply.
+  IOWrite(&VBUS_EN, 0);
+  // ensure Vbus stable before enable Vout with a 5ms delay.
+  vTaskDelay(pdMS_TO_TICKS(5));
+  // enable Vout, 12V by default.
+  IOWrite(&PL_BUCK_EN, 0);
+}
+
+void loop(void) {
+  /* USER LOOP CODE GOES HERE */
+
+  // Read a byte if it is available
+  while (PLUART::byteAvailable()) {
+    uint8_t byte_read = PLUART::readByte();
+    printf("byte: %c\n", (char)byte_read);
+  }
+
+  // Read a line if it is available
+  if (PLUART::lineAvailable()) {
+    uint16_t read_len =
+        PLUART::readLine(payload_buffer, sizeof(payload_buffer));
+    printf("line: %s\n", payload_buffer);
+
+    // Get the RTC if available
+    RTCTimeAndDate_t time_and_date = {};
+    rtcGet(&time_and_date);
+    char rtcTimeBuffer[32];
+    rtcPrint(rtcTimeBuffer, &time_and_date);
+
+    // Print the payload data to a file, to the bm_printf console, and to the printf console.
+    bm_fprintf(0, "payload_data.log", USE_TIMESTAMP, "tick: %llu, rtc: %s, line: %.*s\n",
+               uptimeGetMs(), rtcTimeBuffer, read_len, payload_buffer);
+    bm_printf(0, "[payload] | tick: %llu, rtc: %s, line: %.*s", uptimeGetMs(),
+              rtcTimeBuffer, read_len, payload_buffer);
+    printf("[payload] | tick: %llu, rtc: %s, line: %.*s\n", uptimeGetMs(),
+           rtcTimeBuffer, read_len, payload_buffer);
+  }
+}
---
+++ src/bsp/bm_mote_rs232/Core/Src/gpio.c
@@ -0,0 +1,135 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+void MX_GPIO_Init(void)
+{
+
+  LL_EXTI_InitTypeDef EXTI_InitStruct = {0};
+  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOC);
+  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOH);
+  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOA);
+  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOB);
+
+  /**/
+  LL_GPIO_ResetOutputPin(ADIN_PWR_GPIO_Port, ADIN_PWR_Pin);
+
+  /**/
+  LL_GPIO_ResetOutputPin(GPIOA, ADIN_RST_Pin|PL_BUCK_EN_Pin|FLASH_CS_Pin|ADIN_CS_Pin);
+
+  /**/
+  LL_GPIO_ResetOutputPin(GPIOB, DISCHARGE_ON_Pin|VBUS_EN_Pin);
+
+  /**/
+  GPIO_InitStruct.Pin = ADIN_PWR_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(ADIN_PWR_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = ADIN_RST_Pin|PL_BUCK_EN_Pin|FLASH_CS_Pin|ADIN_CS_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = DISCHARGE_ON_Pin|VBUS_EN_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = BOOT_LED_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(BOOT_LED_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  LL_EXTI_SetEXTISource(LL_EXTI_EXTI_PORTA, LL_EXTI_EXTI_LINE9);
+
+  /**/
+  LL_EXTI_SetEXTISource(LL_EXTI_EXTI_PORTB, LL_EXTI_EXTI_LINE8);
+
+  /**/
+  EXTI_InitStruct.Line_0_31 = LL_EXTI_LINE_9;
+  EXTI_InitStruct.LineCommand = ENABLE;
+  EXTI_InitStruct.Mode = LL_EXTI_MODE_IT;
+  EXTI_InitStruct.Trigger = LL_EXTI_TRIGGER_RISING_FALLING;
+  LL_EXTI_Init(&EXTI_InitStruct);
+
+  /**/
+  EXTI_InitStruct.Line_0_31 = LL_EXTI_LINE_8;
+  EXTI_InitStruct.LineCommand = ENABLE;
+  EXTI_InitStruct.Mode = LL_EXTI_MODE_IT;
+  EXTI_InitStruct.Trigger = LL_EXTI_TRIGGER_FALLING;
+  LL_EXTI_Init(&EXTI_InitStruct);
+
+  /**/
+  LL_GPIO_SetPinPull(VUSB_DETECT_GPIO_Port, VUSB_DETECT_Pin, LL_GPIO_PULL_NO);
+
+  /**/
+  LL_GPIO_SetPinPull(ADIN_INT_GPIO_Port, ADIN_INT_Pin, LL_GPIO_PULL_NO);
+
+  /**/
+  LL_GPIO_SetPinMode(VUSB_DETECT_GPIO_Port, VUSB_DETECT_Pin, LL_GPIO_MODE_INPUT);
+
+  /**/
+  LL_GPIO_SetPinMode(ADIN_INT_GPIO_Port, ADIN_INT_Pin, LL_GPIO_MODE_INPUT);
+
+  /* EXTI interrupt init*/
+  NVIC_SetPriority(EXTI8_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),6, 0));
+  NVIC_EnableIRQ(EXTI8_IRQn);
+  NVIC_SetPriority(EXTI9_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),6, 0));
+  NVIC_EnableIRQ(EXTI9_IRQn);
+
+}
+
+/* USER CODE BEGIN 2 */
+
+/* USER CODE END 2 */
