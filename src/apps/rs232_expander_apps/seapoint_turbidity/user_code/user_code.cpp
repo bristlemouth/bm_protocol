93a6e51a4b2897585452e438eee7bd068f44fd41
Victor Sowa <victor.sowa@sofarocean.com>
2024-09-19 15:27:19 -0700

    port the seapoint turbidity sensor over to rs232 expander board

---
+++ src/apps/rs232_expander_apps/seapoint_turbidity/user_code/user_code.cpp
@@ -0,0 +1,48 @@
+#include "user_code.h"
+#include "FreeRTOS.h"
+#include "bm_seapoint_turbidity_data_msg.h"
+#include "bm_printf.h"
+#include "bm_pubsub.h"
+#include "bsp.h"
+#include "debug.h"
+#include "device_info.h"
+#include "seapoint_turbidity_sensor.h"
+#include "uptime.h"
+#include "util.h"
+
+static constexpr uint32_t BM_SEAPOINT_TURBIDITY_DATA_MSG_MAX_SIZE = 256;
+
+extern cfg::Configuration *systemConfigurationPartition;
+static SeapointTurbiditySensor seapoint_turbidity_sensor;
+static char seapoint_turbidity_topic[BM_TOPIC_MAX_LEN];
+static int seapoint_turbidity_topic_str_len;
+
+static int createSeapointTurbidityDataTopic(void) {
+  int topic_str_len = snprintf(seapoint_turbidity_topic, BM_TOPIC_MAX_LEN,
+                               "sensor/%016" PRIx64 "/sofar/seapoint_turbidity_data", getNodeId());
+  configASSERT(topic_str_len > 0 && topic_str_len < BM_TOPIC_MAX_LEN);
+  return topic_str_len;
+}
+
+void setup(void) {
+  configASSERT(systemConfigurationPartition);
+  seapoint_turbidity_sensor.init();
+  seapoint_turbidity_topic_str_len = createSeapointTurbidityDataTopic();
+  IOWrite(&VBUS_EN, 0);
+  vTaskDelay(pdMS_TO_TICKS(500)); // Wait for Vbus to stabilize
+  IOWrite(&PL_BUCK_EN, 0);
+}
+
+void loop(void) {
+  // Read and handle line from sensor
+  static BmSeapointTurbidityDataMsg::Data d;
+  if (seapoint_turbidity_sensor.getData(d)) {
+    static uint8_t cbor_buf[BM_SEAPOINT_TURBIDITY_DATA_MSG_MAX_SIZE];
+    size_t encoded_len = 0;
+    if (BmSeapointTurbidityDataMsg::encode(d, cbor_buf, sizeof(cbor_buf), &encoded_len) == CborNoError) {
+      bm_pub_wl(seapoint_turbidity_topic, seapoint_turbidity_topic_str_len, cbor_buf, encoded_len, 0);
+    } else {
+      printf("Failed to encode turbidity data message\n");
+    }
+  }
+}
