cd30baa86c51334620537d34a4f603f719d67760
evanShap <evan@sofarocean.com>
2024-09-23 01:32:38 -0700

    LineParser and sub-classes support string value types

--- src/lib/common/OrderedSeparatorLineParser.cpp
+++ src/lib/common/OrderedSeparatorLineParser.cpp
@@ -11,12 +11,19 @@ OrderedSeparatorLineParser::OrderedSeparatorLineParser(const char* separator, si
     : LineParser(separator, maxLineLen, valueTypes, numValues, header) { }
 
 // Parse the value based on its type
+// workStr is a copy of the line - note strtok_r will modify *workStr in-place
 bool OrderedSeparatorLineParser::parseValues(char* workStr) {
-  // Parse the values
+  char* saveptr; // Additional pointer for strtok_r so it stays thread-safe
+  // Parse number of expected values from the string
   for (size_t i = 0; i < _numValues; ++i) {
-    char* saveptr; // Additional pointer for strtok_r
+    // First call to strok_r, workStr will be non-NULL (start of string).
+    //    strtok_r will set first occurence of _separator in workStr to NULL
+    //    set saveptr to the next byte
+    //    then return workStr
+    // Subsequent calls workStr is NULL, telling strtok_r to start search from saveptr
+
     char* token = strtok_r(workStr, _separator, &saveptr);
-    if (token == NULL) {
+    if (token == nullptr) {
       printf("ERR - expected next separator: \"%c\" not found in line!\n", _separator[0]);
       return false; // No separator found
     }
@@ -25,7 +32,7 @@ bool OrderedSeparatorLineParser::parseValues(char* workStr) {
       return false;
     }
     // Prepare for the next strtok_r call
-    workStr = NULL;
+    workStr = nullptr;
   }
 
   return true;
