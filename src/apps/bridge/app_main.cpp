5cfa54a24095d6df7aceace232744bf81ec28696
Victor Sowa <victor.sowa@sofarocean.com>
2024-09-24 12:03:19 -0700

    add in the power sampler code so we can get bridge power over data line measurements

--- src/apps/bridge/app_main.cpp
+++ src/apps/bridge/app_main.cpp
@@ -52,6 +52,8 @@
 #include "ram_partitions.h"
 #include "reportBuilder.h"
 #include "sensorController.h"
+#include "sensorSampler.h"
+#include "sensors.h"
 #include "serial.h"
 #include "serial_console.h"
 #include "stm32_rtc.h"
@@ -151,6 +153,9 @@ SerialHandle_t usbPcap = {
     .preTxCb = NULL,
     .postTxCb = NULL,
 };
+
+uint32_t sys_cfg_sensorsPollIntervalMs = DEFAULT_SENSORS_POLL_MS;
+uint32_t sys_cfg_sensorsCheckIntervalS = DEFAULT_SENSORS_CHECK_S;
 
 extern "C" int main(void) {
 
@@ -367,6 +372,12 @@ static void defaultTask(void *parameters) {
   debugNvmCliInit(&debug_cli_partition, &dfu_partition);
   debugDfuInit(&dfu_partition);
   bcl_init(&dfu_partition, &debug_configuration_user, &debug_configuration_system);
+
+  sensorConfig_t sensorConfig = {.sensorCheckIntervalS = sys_cfg_sensorsCheckIntervalS,
+                                 .sensorsPollIntervalMs = sys_cfg_sensorsPollIntervalMs};
+  sensorSamplerInit(&sensorConfig);
+  // must call sensorsInit after sensorSamplerInit
+  sensorsInit();
 
   printf("Using bridge power controller.\n");
   IOWrite(&BOOST_EN, 1);
---
+++ src/apps/bridge/sensors/sensors.cpp
@@ -0,0 +1,20 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include "bsp.h"
+#include "sensors.h"
+#include "FreeRTOS.h"
+
+// Sensor driver includes
+#include "ina232.h"
+
+// Sampler initialization functions (so we don't need individual headers)
+void powerSamplerInit(INA::INA232 **sensors); // implemented in src/lib/sensor_sampler/powerSampler.cpp
+INA::INA232 debugIna1(&i2c1, I2C_INA_PODL_ADDR);
+INA::INA232 *debugIna[NUM_INA232_DEV] = {
+  &debugIna1,
+};
+
+void sensorsInit() {
+  // Power monitor
+  powerSamplerInit(debugIna);
+}
