68879d35de6729f3bbd45577b9f323ef612a261f
Victor Sowa <victor.sowa@sofarocean.com>
2024-09-26 15:05:32 -0700

    clang-format bcmp_ping.h/.cpp

--- src/lib/bcmp/bcmp_ping.h
+++ src/lib/bcmp/bcmp_ping.h
@@ -2,5 +2,6 @@
 
 #include <stdint.h>
 
-BmErr bcmp_send_ping_request(uint64_t node_id, const void *addr, const uint8_t* payload, uint16_t payload_len);
+BmErr bcmp_send_ping_request(uint64_t node_id, const void *addr, const uint8_t *payload,
+                             uint16_t payload_len);
 BmErr ping_init(void);
