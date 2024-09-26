da1559336ac09a8a9571c145c1ff1e762413c81b
Victor Sowa <victor.sowa@sofarocean.com>
2024-09-26 14:22:08 -0700

    update bcmp_ping (echo) and now its only non-bcmp/core/common dependency is "device_info"

--- src/lib/bcmp/bcmp_ping.h
+++ src/lib/bcmp/bcmp_ping.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #include <stdint.h>
-#include "lwip/ip_addr.h"
 
-err_t bcmp_send_ping_request(uint64_t node_id, const ip_addr_t *addr, const uint8_t* payload, uint16_t payload_len);
-err_t bcmp_send_ping_reply(bcmp_echo_reply_t *echo_reply, const ip_addr_t *addr, uint16_t seq_num);
-err_t bcmp_process_ping_request(bcmp_echo_request_t *echo_req, const ip_addr_t *src, const ip_addr_t *dst, uint16_t seq_num);
-err_t bcmp_process_ping_reply(bcmp_echo_reply_t *echo_reply);
+BmErr bcmp_send_ping_request(uint64_t node_id, const void *addr, const uint8_t* payload, uint16_t payload_len);
+// BmErr bcmp_send_ping_reply(BcmpEchoReply *echo_reply, const void *addr, uint16_t seq_num);
+// BmErr bcmp_process_ping_request(BcmpProcessData data, const void *src, const void *dst, uint16_t seq_num);
+// BmErr bcmp_process_ping_reply(BcmpProcessData data);
+BmErr ping_init(void);
