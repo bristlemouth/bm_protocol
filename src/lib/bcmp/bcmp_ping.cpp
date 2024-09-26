73cbbc19fded1bfff6f2af9f3e40256a83ab1c9c
Victor Sowa <85895527+victorsowa12@users.noreply.github.com>
2024-09-26 15:03:12 -0700

    Update bcmp_ping (#171)
    * update bcmp_ping (echo) and now its only non-bcmp/core/common dependency is "device_info"
    
    * finish using all the new core message types and remove neighbors include
    
    * PR feedback

--- src/lib/bcmp/bcmp_ping.cpp
+++ src/lib/bcmp/bcmp_ping.cpp
@@ -1,12 +1,12 @@
-#include "FreeRTOS.h"
-#include "task.h"
+#include "bm_os.h"
 #include <string.h>
 
 #include "bcmp.h"
-#include "bcmp_neighbors.h"
 #include "bcmp_ping.h"
 #include "device_info.h"
-#include "uptime.h"
+extern "C" {
+#include "packet.h"
+}
 
 static uint64_t _ping_request_time;
 static uint32_t _bcmp_seq;
@@ -20,23 +20,23 @@ static uint16_t _expected_payload_len = 0;
   \param *addr - ip address to send to send ping request to
   \param *payload - payload to send with ping
   \param payload_len - length of payload to send
-  \ret ERR_OK if successful
+  \ret BmOK if successful
 */
-err_t bcmp_send_ping_request(uint64_t node_id, const ip_addr_t *addr, const uint8_t *payload,
+BmErr bcmp_send_ping_request(uint64_t node_id, const void *addr, const uint8_t *payload,
                              uint16_t payload_len) {
 
   if (payload == NULL) {
     payload_len = 0;
   }
 
-  uint16_t echo_len = sizeof(bcmp_echo_request_t) + payload_len;
+  uint16_t echo_len = sizeof(BcmpEchoRequest) + payload_len;
 
-  uint8_t *echo_req_buff = static_cast<uint8_t *>(pvPortMalloc(echo_len));
-  configASSERT(echo_req_buff);
+  uint8_t *echo_req_buff = (uint8_t *)bm_malloc(echo_len);
+  // configASSERT(echo_req_buff);
 
   memset(echo_req_buff, 0, echo_len);
 
-  bcmp_echo_request_t *echo_req = reinterpret_cast<bcmp_echo_request_t *>(echo_req_buff);
+  BcmpEchoRequest *echo_req = (BcmpEchoRequest *)echo_req_buff;
 
   echo_req->target_node_id = node_id;
   echo_req->id = (uint16_t)getNodeId(); // TODO - make this a randomly generated number
@@ -45,7 +45,7 @@ err_t bcmp_send_ping_request(uint64_t node_id, const ip_addr_t *addr, const uint
 
   // clear the expected payload
   if (_expected_payload != NULL) {
-    vPortFree(_expected_payload);
+    bm_free(_expected_payload);
     _expected_payload = NULL;
     _expected_payload_len = 0;
   }
@@ -53,7 +53,7 @@ err_t bcmp_send_ping_request(uint64_t node_id, const ip_addr_t *addr, const uint
   // Lets only copy the paylaod if it isn't NULL just in case
   if (payload != NULL && payload_len > 0) {
     memcpy(&echo_req->payload[0], payload, payload_len);
-    _expected_payload = static_cast<uint8_t *>(pvPortMalloc(payload_len));
+    _expected_payload = (uint8_t *)bm_malloc(payload_len);
     memcpy(_expected_payload, payload, payload_len);
     _expected_payload_len = payload_len;
   }
@@ -61,12 +61,12 @@ err_t bcmp_send_ping_request(uint64_t node_id, const ip_addr_t *addr, const uint
   printf("PING (%016" PRIx64 "): %" PRIu16 " data bytes\n", echo_req->target_node_id,
          echo_req->payload_len);
 
-  err_t rval =
-      bcmp_tx(addr, BcmpEchoRequestMessage, reinterpret_cast<uint8_t *>(echo_req), echo_len, 0);
+  BmErr rval =
+      bcmp_tx(addr, BcmpEchoRequestMessage, (uint8_t *)echo_req, echo_len, 0);
 
-  _ping_request_time = uptimeGetMicroSeconds();
+  _ping_request_time = bm_ticks_to_ms(bm_get_tick_count());
 
-  vPortFree(echo_req_buff);
+  bm_free(echo_req_buff);
 
   return rval;
 }
@@ -76,12 +76,12 @@ err_t bcmp_send_ping_request(uint64_t node_id, const ip_addr_t *addr, const uint
 
   \param *echo_reply -  echo reply message
   \param *addr - ip address to send ping reply to
-  \ret ERR_OK if successful
+  \ret BmOK if successful
 */
-err_t bcmp_send_ping_reply(bcmp_echo_reply_t *echo_reply, const ip_addr_t *addr,
+static BmErr bcmp_send_ping_reply(BcmpEchoReply *echo_reply,  void *addr,
                            uint16_t seq_num) {
 
-  return bcmp_tx(addr, BcmpEchoReplyMessage, reinterpret_cast<uint8_t *>(echo_reply),
+  return bcmp_tx(addr, BcmpEchoReplyMessage, (uint8_t *)echo_reply,
                  sizeof(*echo_reply) + echo_reply->payload_len, seq_num);
 }
 
@@ -91,18 +91,17 @@ err_t bcmp_send_ping_reply(bcmp_echo_reply_t *echo_reply, const ip_addr_t *addr,
   \param *echo_req - echo request message to process
   \param *src - source ip of requester
   \param *dst - destination ip of request (used for responding to the correct multicast address)
-  \ret ERR_OK if successful
+  \ret BmOK if successful
 */
-err_t bcmp_process_ping_request(bcmp_echo_request_t *echo_req, const ip_addr_t *src,
-                                const ip_addr_t *dst, uint16_t seq_num) {
-  (void)src;
-  configASSERT(echo_req);
+static BmErr bcmp_process_ping_request(BcmpProcessData data) {
+  BcmpEchoRequest *echo_req = (BcmpEchoRequest *)data.payload;
+  // configASSERT(echo_req);
   if ((echo_req->target_node_id == 0) || (getNodeId() == echo_req->target_node_id)) {
     echo_req->target_node_id = getNodeId();
-    return bcmp_send_ping_reply(reinterpret_cast<bcmp_echo_reply_t *>(echo_req), dst, seq_num);
+    return bcmp_send_ping_reply((BcmpEchoReply *)echo_req, data.dst, data.seq_num);
   }
 
-  return ERR_OK;
+  return BmOK;
 }
 
 /*!
@@ -113,10 +112,9 @@ err_t bcmp_process_ping_request(bcmp_echo_request_t *echo_req, const ip_addr_t *
   \param *dst - destination ip of request (used for responding to the correct multicast address)
   \
   */
-err_t bcmp_process_ping_reply(bcmp_echo_reply_t *echo_reply) {
-  configASSERT(echo_reply);
-
-  err_t rval = ERR_VAL;
+static BmErr bcmp_process_ping_reply(BcmpProcessData data) {
+  BmErr err = BmEINVAL;
+  BcmpEchoReply *echo_reply = (BcmpEchoReply *)data.payload;
 
   do {
     if (_expected_payload_len != echo_reply->payload_len) {
@@ -134,13 +132,34 @@ err_t bcmp_process_ping_reply(bcmp_echo_reply_t *echo_reply) {
       }
     }
 
-    uint64_t diff = uptimeGetMicroSeconds() - _ping_request_time;
+    uint64_t diff = bm_ticks_to_ms(bm_get_tick_count()) - _ping_request_time;
     printf("🏓 %" PRIu16 " bytes from %016" PRIx64 " bcmp_seq=%" PRIu32 " time=%" PRIu64
            " ms\n",
-           echo_reply->payload_len, echo_reply->node_id, echo_reply->seq_num, diff / 1000);
-    rval = ERR_OK;
+           echo_reply->payload_len, echo_reply->node_id, echo_reply->seq_num, diff);
+    err = BmOK;
 
   } while (0);
 
-  return rval;
+  return err;
+}
+
+
+BmErr ping_init(void) {
+  BcmpPacketCfg ping_request = {
+      false,
+      false,
+      bcmp_process_ping_request,
+  };
+  BcmpPacketCfg ping_reply = {
+      false,
+      false,
+      bcmp_process_ping_reply,
+  };
+
+  BmErr err;
+  err = packet_add(&ping_request, BcmpEchoRequestMessage);
+  if (err == BmOK) {
+    err = packet_add(&ping_reply, BcmpEchoReplyMessage);
+  }
+  return err;
 }
