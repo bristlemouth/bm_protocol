861efdd82d63d93cff41a66a5c1b45b98a50c30a
evanShap <evan@sofarocean.com>
2024-09-27 15:40:24 -0700

    rx live user code tweaks & comments

--- src/apps/bm_devkit/rx_live_demo/user_code/user_code.cpp
+++ src/apps/bm_devkit/rx_live_demo/user_code/user_code.cpp
@@ -17,84 +17,104 @@
 
 #define LED_ON_TIME_MS 20
 #define LED_PERIOD_MS 1000
+// NOTE - we manually set our Rx-LIVE to 115200 in a one-time setup.
 #define DEFAULT_BAUD_RATE 115200
-/// NOTE - the RxLive uses Carriage Return for line term for incoming commands.
-/// It will terminate response lines with \r\n
-/// TODO - allow configuration of different Tx term and multi-char Rx line breaks
+// NOTE - the Rx-LIVE uses Carriage Return for line term for incoming commands.
+// It will terminate response lines with \r\n
+// We're just using the first \r char, so the next line will have the \n at the start.
+/// TODO - allow configuration of different Tx and Rx line terminations.
+/// TODO - allow for multi-charater terminations.
 #define DEFAULT_LINE_TERM 13 // CR / '\r', 0x0D
 
+// Size of the structure we'll use to report detection counts for each unique tag.
 #define RX_LIVE_DETECTION_SIZE sizeof(TagID)
+// We'll store up to 300 bytes worth of tags, this ends up being 27 currently.
 #define MAX_TAGS (uint16_t)(300 / RX_LIVE_DETECTION_SIZE)
+// We'll add another uint32_t to our report for the Status Lines
 #define MAX_TX_BUFFER_SIZE (MAX_TAGS * RX_LIVE_DETECTION_SIZE + sizeof(uint32_t))
-#define DEFALUT_SAMPLE_DUR_S 180
+// Sample for 3 minutes, then report all tag detections and Status Lines
+#define DEFAULT_SAMPLE_DUR_S 180
 
 // app_main passes a handle to the user config partition in NVM.
 extern cfg::Configuration *userConfigurationPartition;
 
 // The serial number of our Rx-Live receiver, loaded from user config partition.
 uint32_t rx_live_serial_number = 0;
-static RxLiveSensor rx_live;    // Class instance for our Rx-Live.
-uint32_t rx_live_sts_count = 0; // Counter for status updates received in the session
-uint32_t unique_tag_ids = 0;
+static RxLiveSensor rx_live;  // Class instance for our Rx-Live.
+uint32_t rx_live_sts_count = 0;  // Counter for status updates received in the session
+uint32_t unique_tag_ids = 0;  // Number of unique tag IDs we've seen this session
 TagID tag_detections[MAX_TAGS] = {};  // Array to store unique Tag IDs + detection counts
-uint8_t tx_buffer[MAX_TX_BUFFER_SIZE];   // Buffer for data transmission
-uint32_t sample_duration_s = DEFALUT_SAMPLE_DUR_S;
+uint8_t tx_buffer[MAX_TX_BUFFER_SIZE];  // Buffer for data transmission
+uint32_t sample_duration_s = DEFAULT_SAMPLE_DUR_S;  // Sample duration loaded from user config if set
 uint32_t sample_duration_ms = sample_duration_s * 1000;
-uint32_t _sample_timer_ms = 0;
+uint32_t _sample_timer_ms = 0;  // Timer variable to trigger a report
 
 // A timer variable we can set to trigger a pulse on LED2 when we get payload serial data
 static int32_t ledLinePulse = -1;
 
-// A buffer for our data from the payload uart
+// A buffer for our Rx data from the payload uart
 char payload_buffer[2048];
 static u_int32_t baud_rate_config = DEFAULT_BAUD_RATE;
 static u_int32_t line_term_config = DEFAULT_LINE_TERM;
 
+/**
+ * @brief Reports the count of detections per unique tag, as well as total count of Status Lines
+ * received from the Rx-LIVE.
+ *
+ * It logs this data to Spotter SD card and
+ * reports it over tx-data telemetry.
+ *
+ * We expect 1 Status Line at boot and then 1 per minute. The Status
+ * Line count gives us an overall indication the sensor was connected and working as expected
+ * during the entire sample duration.
+ *
+ */
 void report_detections() {
-  memset(tx_buffer, 0, sizeof(tx_buffer));
-  // Copy rx_live_sts_count into the first 4 bytes of tx_buffer
-  memcpy(tx_buffer, &rx_live_sts_count, sizeof(rx_live_sts_count));
-  // Offset starts after the count
-  size_t tx_len = sizeof(rx_live_sts_count);
-  // Copy populated tag_detections into tx_buffer
-  for (uint32_t i = 0; i < unique_tag_ids; ++i) {
-    // Copy the TagID struct directly into the buffer
+  memset(tx_buffer, 0, sizeof(tx_buffer)); //  Clear the telemetry data buffer.
+  memcpy(tx_buffer, &rx_live_sts_count, sizeof(rx_live_sts_count));  // Copy rx_live_sts_count into the first 4 bytes of tx_buffer.
+  size_t tx_len = sizeof(rx_live_sts_count);  // Keep track of the total number of valid bytes in the buffer.
+  // Copy all tag_detections into the buffer.
+  for (size_t i = 0; i < unique_tag_ids; ++i) {
     memcpy(tx_buffer + tx_len, &tag_detections[i], sizeof(TagID));
     tx_len += sizeof(TagID);
   }
 
-  // Get the RTC if available
+  // Get the datetime from the RTC if available, useful for logging
   RTCTimeAndDate_t time_and_date = {};
   rtcGet(&time_and_date);
   char rtcTimeBuffer[32];
   rtcPrint(rtcTimeBuffer, &time_and_date);
 
+  // Print hex string of the buffer to the console for debugging.
   static char tx_hex[MAX_TX_BUFFER_SIZE * 2 + 1];
-  size_t i = 0;
+  size_t i;
   for (i = 0; i < tx_len; ++i) {
     sprintf(&tx_hex[i * 2], "%02X", tx_buffer[i]);
   }
   tx_hex[i * 2] = '\0'; // Null-terminate the string
-
-  // TODO - verify BM-TX log still works and don't need these.
-  // bm_fprintf(0, "rx_live_agg.log", USE_TIMESTAMP, "tick: %llu, rtc: %s, buff: 0x%s\n", uptimeGetMs(), rtcTimeBuffer, tx_hex);
-  // bm_printf(0, "[rx-live-agg] | tick: %llu, rtc: %s, buff: 0x%s", uptimeGetMs(), rtcTimeBuffer, tx_hex);
-  printf("[rx-live-agg] | tick: %llu, rtc: %s, buff: %s\n", uptimeGetMs(), rtcTimeBuffer, tx_hex);
-
+  bm_printf(0, "[rx-live-agg] detection buffer to send | tick: %llu, rtc: %s, buff: 0x%s", uptimeGetMs(), rtcTimeBuffer, tx_hex);
+  printf("[rx-live-agg] detection buffer to send | tick: %llu, rtc: %s, buff: %s\n", uptimeGetMs(), rtcTimeBuffer, tx_hex);
+  // Transmit over Spotter celluar or Iridium SBD fallback.
   if (spotter_tx_data(tx_buffer, tx_len, BM_NETWORK_TYPE_CELLULAR_IRI_FALLBACK)) {
-    printf("%llut - %s | Sucessfully sent Spotter transmit data request\n", uptimeGetMs(),
-           rtcTimeBuffer);
+    printf("%llut - %s | Sucessfully sent Spotter transmit data request\n", uptimeGetMs(), rtcTimeBuffer);
   } else {
-    printf("%llut - %s | Failed to send Spotter transmit data request\n", uptimeGetMs(),
-           rtcTimeBuffer);
+    printf("%llut - %s | Failed to send Spotter transmit data request\n", uptimeGetMs(), rtcTimeBuffer);
   }
-
+  // Reset the counts and the tag detections array for the next collection period.
   rx_live_sts_count = 0;
   unique_tag_ids = 0;
   memset(tag_detections, 0, sizeof(tag_detections));
 }
 
-// Function to tally detections
+/**
+ * @brief Tally the tag detections and update detection counts and buffer.
+ *
+ * This function searches the current list of detected tags to find a match for the
+ * given `latest_detection`. If a match is found, the detection count is incremented.
+ * Otherwise, the new detection is added to the list, and the list is printed.
+ *
+ * @param latest_detection The latest tag detection to be tallied.
+ */
 void tally_detection(const TagID &latest_detection) {
   bool found = false;
   // Iterate through existing tag detections to find a match
@@ -106,8 +126,8 @@ void tally_detection(const TagID &latest_detection) {
       if (tag_detections[i].detection_count < UINT16_MAX) {
         tag_detections[i].detection_count++;
       } else {
-        // Optionally, print a warning or handle the case when max is reached
-        printf("Warning: Detection count for tag serial no %u has reached its maximum value (%u).\n",
+        // Print an error for debugging
+        printf("Error: Detection count for tag serial no %u has reached its maximum value (%u).\n",
                tag_detections[i].tag_serial_no, UINT16_MAX);
       }
       found = true;
@@ -121,13 +141,13 @@ void tally_detection(const TagID &latest_detection) {
       tag_detections[unique_tag_ids].detection_count = 1; // Initialize count to 1
       unique_tag_ids++;
     } else {
-      // Handle the case where we've reached the maximum number of tags
+      // Print an error for debugging if no more room for new tags
       printf("Error: Maximum number of unique tags (%d) reached.\n", MAX_TAGS);
       return;
     }
   }
 
-  // Nicely print the whole list of tag detections
+  // Nicely print the whole list of tag detections to the console
   printf("Current Tag Detections:\n");
   printf("---------------------------------------------------------\n");
   printf("| %-5s | %-10s | %-6s | %-8s | %-5s | %-10s |\n",
@@ -148,11 +168,15 @@ void tally_detection(const TagID &latest_detection) {
 void setup() {
   /* USER ONE-TIME SETUP CODE GOES HERE */
 
-  // Retrieve user-set config values out of NVM.
+  // Retrieve config values from the user partition of Non-Volatile Memory.
   configASSERT(userConfigurationPartition);
+  // Baud rate for the payload UART interface
   userConfigurationPartition->getConfig("plUartBaudRate", strlen("plUartBaudRate"), baud_rate_config);
+  // Line termination character for the payload UART interface
   userConfigurationPartition->getConfig("plUartLineTerm", strlen("plUartLineTerm"), line_term_config);
+  // The serial number of the connected Rx-LIVE receiver.
   userConfigurationPartition->getConfig("rxLiveSerial", strlen("rxLiveSerial"), rx_live_serial_number);
+  // The sample duration in seconds, at which we'll report all tag detection and Status Line counts
   userConfigurationPartition->getConfig("sampleDurS", strlen("sampleDurS"), sample_duration_s);
 
   sample_duration_ms = sample_duration_s * 1000;
@@ -205,8 +229,9 @@ void loop() {
     printf("[rx-live-raw] | tick: %llu, rtc: %s, line: %.*s\n", uptimeGetMs(),
            rtcTimeBuffer, read_len, payload_buffer);
 
-    // run the line the the RxLive state machine and parser
+    // pass the line into the Rx-LIVE state machine and parser.
     RxLiveStatusCode rx_code = rx_live.run((const uint8_t*)payload_buffer, read_len);
+    // If we receive a return code that indicates an interesting state change or error, log it for testing and debugging.
     if (rx_code > RX_CODE_INFO) {
       bm_fprintf(0, "rx_live_state.log", USE_TIMESTAMP, "tick: %llu, rtc: %s, rx_code: %d\n",
            uptimeGetMs(), rtcTimeBuffer, rx_code);
@@ -216,19 +241,22 @@ void loop() {
              rtcTimeBuffer, rx_code);
     }
     if (rx_code == RX_CODE_DETECTION) {
+      // If we received a Tag Detection Line, update our tag counts buffer.
       printf("[rx-live-state] :: DETECTION!\n");
       TagID latest_detection = rx_live.getLatestDetection();
       tally_detection(latest_detection);
     } else if (rx_code == RX_CODE_STS) {
+      // If we received a Status Line, update our status line count.
+      // We should expect to see 1 of these per minute if everything is working as expected.
       rx_live_sts_count++;
       printf("[rx-live-state] :: %lu STS polls received in session.\n", rx_live_sts_count);
     }
     ledLinePulse = uptimeGetMs(); // trigger a pulse on LED2
-
   } else { // No new UART Rx line available.
     // Run RxLive state machine without Rx data to service the state machine timers
     RxLiveStatusCode rx_code = rx_live.run(nullptr, 0);
-    if (rx_code > RX_CODE_INFO) { // Something interesting happened in the state machine
+    // An interesting state change or an error
+    if (rx_code > RX_CODE_INFO) {
       // Get the RTC time (if available) for use in log outputs
       RTCTimeAndDate_t time_and_date = {};
       rtcGet(&time_and_date);
