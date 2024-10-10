50bc57e98fd48a9966c9c12df2ed1d44d8d49972
Victor Sowa <victor.sowa@sofarocean.com>
2024-10-10 13:35:52 -0700

    remove final pre-move to bm_core todo (i think)

--- src/lib/bcmp/dfu/bm_dfu_host.cpp
+++ src/lib/bcmp/dfu/bm_dfu_host.cpp
@@ -5,13 +5,8 @@
 #include "bm_dfu_generic.h"
 #include "bm_dfu_host.h"
 
-//TODO - abstract this??? Move this into bm_core or bm_common???
-// This is essentially its own task that is handling the timer callbacks
 extern "C" {
 #include "timer_callback_handler.h"
-}
-
-extern "C" {
 #include "device.h"
 }
 
