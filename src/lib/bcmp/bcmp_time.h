01426bba5478b737d4077be9fcd132e2b423d0c3
Victor Sowa <85895527+victorsowa12@users.noreply.github.com>
2024-10-11 14:10:21 -0700

    Remove time from bm_protocol and put into bm_core (#186)
    * the return needs to start as BmOK (+10 squashed commits)
    Squashed commits:
    [b949ad8] use the rtos abstraction
    [2dbf9cf] remove configuration.h includes and old commented out code
    [82239da] it compiles!!
    [67c28b6] try casting it
    [74a5cd6] almost got the configs compiling, there is a dually defined enum that needs to be consolidated... it is defined in both bm_common_messages and bm_core right now.
    [480b176] move the rtc wrapper stuff to its own file
    [7afcaff] compare against bmerr not freertos errors!
    [c25f6e8] cast to the old ip addr type for these non-bcmp LWIP calls
    [df8dee7] okay I've got the bm_rtc stuff working, but now I need to figure out why the multicast_ll_addr is not being found
    [48567c0] try using the bm_rtc functions. not working yet, compiler errors and missing inclusions. looking into that
    
    * we don't need to pass the usr_cfg through anymore
    
    * try with this version of pbuf stuff in bm_lwip.c (+2 squashed commits)
    Squashed commits:
    [5617e7f] trying to get dfu working... almost there, just running into some pbuf issues
    [6c74802] use the generic header file and wrapper functions
    
    * add comment so we can get a different sha for testing!
    
    * add some todos and update bm_core again
    
    * move the dfu_confirm stuff to bm_dfu_generic/wrapper and remove all sys_cfg stuff from dfu! Next we just need to tackle the NVM parition stuff!
    
    * few more sys config changes
    
    * rename the functions to add client and make the dfu_partition a global so the wrapper can access it and call the read function.
    
    * remove the pass through of the the NVM dfu_partition. it is compiling! now for some testing!
    
    * use the copy/modified lib_state_machine.h/.c files from bm_common
    
    * fix a copy paste bug
    
    * remove all references to configuration.h and nvmPartition.h in bcmp code. Highlights the need for Cbor as a submodule in bm_core!
    
    * use the bm_cbor_type_to_config_type, also I guess bm_core can pull the cbor.h from bm_protocol?!?
    
    * here is the commit that adds cbor.h include to bm_core + remove one last nvm include that is no longer needed
    
    * temporarily comment these files out so they don't do anything
    
    * update bm_core and rename old state machine files so they are not included
    
    * get a different git sha for testing!
    
    * clang format
    
    * change the functions to match the latest style guides
    
    * include as externed c
    
    * removing some commented out code
    
    * remove this commented out function since it was moved to bm_dfu_core
    
    * start using the bm_os instead of freertos specifics. still need to figure out what to do with configASSERT. added some todo's for other things that need to be handled, like task priorities.
    
    * move the dfu image defines to the structs header
    
    * use the new device.h
    
    * remove todo
    
    * remove some of the todo's that got done
    
    * remove all freertos from bm_dfu_host.h/.cpp!
    
    * remove all freertos from bm_dfu_core!
    
    * remove freertos comment
    
    * moved crc to bm_common
    
    * moved the task priority define to bm_dfu_generic.h
    
    * use the timer_callback_handler.h/.c that is now in bm_common
    
    * remove final pre-move to bm_core todo (i think)
    
    * need to compare to BmOK
    
    * commit merge conflict changes and add config assert todo
    
    * move the extern to bm_common
    
    * point back to main
    
    * remove files that were already moved over
    
    * these should be in bm_common now
    
    * remove time from bm_protocol

--- src/lib/bcmp/bcmp_time.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdint.h>
-extern "C" {
-#include "util.h"
-}
-
-BmErr bcmp_time_set_time(uint64_t target_node_id, uint64_t utc_us);
-BmErr bcmp_time_get_time(uint64_t target_node_id);
-BmErr time_init(void);
