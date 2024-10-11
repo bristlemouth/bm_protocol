694aefa4fa6f57fbf8615efc8ceeeff7c1043e68
Matthew Krause <61891357+matt001k@users.noreply.github.com>
2024-10-11 16:55:36 -0700

    Ports L2 Module To bm_core (#184)
    * Ports L2 Module To bm_core
    
    Refactors code to utilize bm_core API (bm_os, etc.)
    Abstracts lwip elements from l2 file and provides a generic API that can be used across multiple network stacks
    Re-format Code To Match Coding Standard

--- src/lib/bcmp/bm/bristlemouth.cpp
+++ src/lib/bcmp/bm/bristlemouth.cpp
@@ -1,5 +1,3 @@
-#include "bristlemouth.h"
-
 // Includes for FreeRTOS
 #include "FreeRTOS.h"
 #include "bcmp_messages.h"
@@ -8,17 +6,12 @@
 #include "queue.h"
 #include "task.h"
 
-#include "lwip/inet.h"
-#include "lwip/init.h"
-#include "lwip/mld6.h"
-#include "lwip/tcpip.h"
-#include "lwip/udp.h"
-
 #include "bm_l2.h"
 #include "bm_ports.h"
 
 extern "C" {
 #include "bcmp.h"
+#include "bm_ip.h"
 #include "device.h"
 }
 #include "bcmp_cli.h"
@@ -43,50 +36,6 @@ void bm_link_change_cb(uint8_t port, bool state) {
 }
 
 void bcl_init(void) {
-  err_t mld6_err;
-  // int         rval;
-  ip6_addr_t multicast_glob_addr;
-
-  printf("Starting up BCL\n");
-
-  /* Looks like we don't call lwip_init if we are using a RTOS */
-  tcpip_init(NULL, NULL);
-
-  getMacAddr(netif.hwaddr, sizeof(netif.hwaddr));
-  netif.hwaddr_len = sizeof(netif.hwaddr);
-
-  inet6_aton("ff03::1", &multicast_glob_addr);
-
-  // Initialize l2 structs/callbacks
-  bm_l2_init(bm_link_change_cb);
-
-  // The documentation says to use tcpip_input if we are running with an OS
-  // bm_l2_netif_init will be called from netif_add
-  netif_add(&netif, NULL, bm_l2_netif_init, tcpip_input);
-
-  // Create IP addresses from node_id
-  const uint64_t node_id = getNodeId();
-  ip_addr_t unicast_addr =
-      IPADDR6_INIT_HOST(0xFD000000, 0, (node_id >> 32) & 0xFFFFFFFF, node_id & 0xFFFFFFFF);
-
-  ip_addr_t ll_addr =
-      IPADDR6_INIT_HOST(0xFE800000, 0, (node_id >> 32) & 0xFFFFFFFF, node_id & 0xFFFFFFFF);
-
-  // Generate IPv6 address using EUI-64 format derived from mac address
-  netif_ip6_addr_set(&netif, 0, ip_2_ip6(&ll_addr));
-  netif_ip6_addr_set(&netif, 1, ip_2_ip6(&unicast_addr));
-  netif_set_up(&netif);
-  netif_ip6_addr_set_state(&netif, 0, IP6_ADDR_VALID);
-  netif_ip6_addr_set_state(&netif, 1, IP6_ADDR_VALID);
-
-  netif_set_link_up(&netif);
-
-  /* add to relevant multicast groups */
-  mld6_err = mld6_joingroup_netif(&netif, &multicast_glob_addr);
-  if (mld6_err != ERR_OK) {
-    printf("Could not join ff03::1\n");
-  }
-
   //TODO: This should all be moved to user code
   uint8_t major, minor, patch;
   getFWVersion(&major, &minor, &patch);
@@ -103,12 +52,19 @@ void bcl_init(void) {
       .ver_patch = patch,
       .sn = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'},
   };
+
+  printf("Starting up BCL\n");
   device_init(device);
+
+  bm_l2_init(bm_link_change_cb);
+
+  bm_ip_init();
+
+  bcmp_init();
   //TODO: These items need to be moved into bcmp again once we figure out how
   //      to get partitions/configurations configurable by the user
   //bm_dfu_init(bcmp_dfu_tx, dfu_partition, sys_cfg);
 
-  bcmp_init();
   bcmp_cli_init();
 
   bm_middleware_init(&netif, BM_MIDDLEWARE_PORT);
@@ -119,13 +75,9 @@ void bcl_init(void) {
 }
 
 /*!
-  Get string representation of IP address.
-  NOTE: returns ptr to static buffer; not reentrant! (from ip6addr_ntoa)
+  @brief Get string representation of IP address.
 
-  \param[in] ip IP address index (Since there are up to LWIP_IPV6_NUM_ADDRESSES)
-  \return pointer to string with ip address
+  @param[in] ip IP address index
+  @return pointer to string with ip address
 */
-const char *bcl_get_ip_str(uint8_t idx) {
-  configASSERT(idx < LWIP_IPV6_NUM_ADDRESSES);
-  return (ip6addr_ntoa(netif_ip6_addr(&netif, idx)));
-}
+const char *bcl_get_ip_str(uint8_t idx) { return bm_ip_get_str(idx); }
--- src/lib/bcmp/bm/bristlemouth.h
+++ src/lib/bcmp/bm/bristlemouth.h
@@ -1,21 +1,6 @@
 #pragma once
 
-#include <stddef.h>
 #include <stdint.h>
-
-#include "lwip/pbuf.h"
-#include "lwip/tcpip.h"
-#include "lwip/inet.h"
-
-#include "serial.h"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 void bcl_init(void);
 const char *bcl_get_ip_str(uint8_t ip);
-
-#ifdef __cplusplus
-}
-#endif
