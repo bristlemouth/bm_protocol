ca3aec8785aca04825de9a66138331e6da9a1838
Victor Sowa <85895527+victorsowa12@users.noreply.github.com>
2024-10-11 12:47:33 -0700

    Experiment/use bm os in dfu (#185)
    * the return needs to start as BmOK (+10 squashed commits)
    Squashed commits:
    [b949ad8] use the rtos abstraction
    [2dbf9cf] remove configuration.h includes and old commented out code
    [82239da] it compiles!!
    [67c28b6] try casting it
    [74a5cd6] almost got the configs compiling, there is a dually defined enum that needs to be consolidated... it is defined in both bm_common_messages and bm_core right now.
    [480b176] move the rtc wrapper stuff to its own file
    [7afcaff] compare against bmerr not freertos errors!
    [c25f6e8] cast to the old ip addr type for these non-bcmp LWIP calls
    [df8dee7] okay I've got the bm_rtc stuff working, but now I need to figure out why the multicast_ll_addr is not being found
    [48567c0] try using the bm_rtc functions. not working yet, compiler errors and missing inclusions. looking into that
    
    * we don't need to pass the usr_cfg through anymore
    
    * try with this version of pbuf stuff in bm_lwip.c (+2 squashed commits)
    Squashed commits:
    [5617e7f] trying to get dfu working... almost there, just running into some pbuf issues
    [6c74802] use the generic header file and wrapper functions
    
    * add comment so we can get a different sha for testing!
    
    * add some todos and update bm_core again
    
    * move the dfu_confirm stuff to bm_dfu_generic/wrapper and remove all sys_cfg stuff from dfu! Next we just need to tackle the NVM parition stuff!
    
    * few more sys config changes
    
    * rename the functions to add client and make the dfu_partition a global so the wrapper can access it and call the read function.
    
    * remove the pass through of the the NVM dfu_partition. it is compiling! now for some testing!
    
    * use the copy/modified lib_state_machine.h/.c files from bm_common
    
    * fix a copy paste bug
    
    * remove all references to configuration.h and nvmPartition.h in bcmp code. Highlights the need for Cbor as a submodule in bm_core!
    
    * use the bm_cbor_type_to_config_type, also I guess bm_core can pull the cbor.h from bm_protocol?!?
    
    * here is the commit that adds cbor.h include to bm_core + remove one last nvm include that is no longer needed
    
    * temporarily comment these files out so they don't do anything
    
    * update bm_core and rename old state machine files so they are not included
    
    * get a different git sha for testing!
    
    * clang format
    
    * change the functions to match the latest style guides
    
    * include as externed c
    
    * removing some commented out code
    
    * remove this commented out function since it was moved to bm_dfu_core
    
    * start using the bm_os instead of freertos specifics. still need to figure out what to do with configASSERT. added some todo's for other things that need to be handled, like task priorities.
    
    * move the dfu image defines to the structs header
    
    * use the new device.h
    
    * remove todo
    
    * remove some of the todo's that got done
    
    * remove all freertos from bm_dfu_host.h/.cpp!
    
    * remove all freertos from bm_dfu_core!
    
    * remove freertos comment
    
    * moved crc to bm_common
    
    * moved the task priority define to bm_dfu_generic.h
    
    * use the timer_callback_handler.h/.c that is now in bm_common
    
    * remove final pre-move to bm_core todo (i think)
    
    * need to compare to BmOK
    
    * commit merge conflict changes and add config assert todo
    
    * move the extern to bm_common
    
    * point back to main
    
    * remove files that were already moved over
    
    * these should be in bm_common now

--- src/lib/bcmp/dfu/bm_dfu_host.cpp
+++ src/lib/bcmp/dfu/bm_dfu_host.cpp
@@ -1,25 +1,27 @@
 #include <stdio.h>
 #include <string.h>
+#include "bm_os.h"
 #include "bm_dfu.h"
 #include "bm_dfu_generic.h"
 #include "bm_dfu_host.h"
-#include "device_info.h"
-#include "external_flash_partitions.h"
-#include "FreeRTOS.h"
+
+extern "C" {
 #include "timer_callback_handler.h"
+#include "device.h"
+}
 
 typedef struct dfu_host_ctx_t {
-    QueueHandle_t dfu_event_queue;
-    TimerHandle_t ack_timer;
+    BmQueue dfu_event_queue;
+    BmTimer ack_timer;
     uint8_t ack_retry_num;
-    TimerHandle_t heartbeat_timer;
+    BmTimer heartbeat_timer;
     bm_dfu_img_info_t img_info;
     uint64_t self_node_id;
     uint64_t client_node_id;
     bcmp_dfu_tx_func_t bcmp_dfu_tx;
     uint32_t bytes_remaining;
     update_finish_cb_t update_complete_callback;
-    TimerHandle_t update_timer;
+    BmTimer update_timer;
     uint32_t host_timeout_ms;
 } dfu_host_ctx_t;
 
@@ -27,9 +29,9 @@ static constexpr uint32_t FLASH_READ_TIMEOUT_MS = 5 * 1000;
 
 static dfu_host_ctx_t host_ctx;
 
-static void ack_timer_handler(TimerHandle_t tmr);
-static void heartbeat_timer_handler(TimerHandle_t tmr);
-static void update_timer_handler(TimerHandle_t tmr);
+static void ack_timer_handler(BmTimer tmr);
+static void heartbeat_timer_handler(BmTimer tmr);
+static void update_timer_handler(BmTimer tmr);
 
 static void bm_dfu_host_req_update();
 static void bm_dfu_host_send_reboot();
@@ -44,12 +46,14 @@ static void bm_dfu_host_start_update_timer(uint32_t timeoutMs);
  * @param *tmr    Pointer to Timer struct
  * @return none
  */
-static void ack_timer_handler(TimerHandle_t tmr) {
+static void ack_timer_handler(BmTimer tmr) {
     (void) tmr;
     bm_dfu_event_t evt = {DFU_EVENT_ACK_TIMEOUT, NULL,0};
 
-    if(xQueueSend(host_ctx.dfu_event_queue, &evt, 0) != pdTRUE) {
-        configASSERT(false);
+    if(bm_queue_send(host_ctx.dfu_event_queue, &evt, 0) != BmOK) {
+        // configASSERT(false);
+        // TODO - handle this better?
+        printf("Failed to send ACK timeout event\n");
     }
 }
 
@@ -74,7 +78,7 @@ static void send_hb_timer_cb(void *arg) {
  * @param *tmr    Pointer to Timer struct
  * @return none
  */
-static void heartbeat_timer_handler(TimerHandle_t tmr) {
+static void heartbeat_timer_handler(BmTimer tmr) {
     (void) tmr;
     timer_callback_handler_send_cb(send_hb_timer_cb,NULL,0);
 }
@@ -86,12 +90,12 @@ static void heartbeat_timer_handler(TimerHandle_t tmr) {
  *
  * @return none
  */
-static void update_timer_handler(TimerHandle_t tmr) {
+static void update_timer_handler(BmTimer tmr) {
     (void) tmr;
     bm_dfu_event_t evt = {DFU_EVENT_ABORT, NULL,0};
 
-    if(xQueueSend(host_ctx.dfu_event_queue, &evt, 0) != pdTRUE) {
-        configASSERT(false);
+    if(bm_queue_send(host_ctx.dfu_event_queue, &evt, 0) != BmOK) {
+        // configASSERT(false);
     }
 }
 
@@ -112,7 +116,7 @@ static void bm_dfu_host_req_update() {
     update_start_req_evt.info.addresses.src_node_id = host_ctx.self_node_id;
     update_start_req_evt.info.addresses.dst_node_id = host_ctx.client_node_id;
     update_start_req_evt.header.frame_type = BCMP_DFU_START;
-    if(host_ctx.bcmp_dfu_tx(static_cast<bcmp_message_type_t>(update_start_req_evt.header.frame_type), reinterpret_cast<uint8_t *>(&update_start_req_evt), sizeof(update_start_req_evt))){
+    if(host_ctx.bcmp_dfu_tx((bcmp_message_type_t)(update_start_req_evt.header.frame_type), (uint8_t *)(&update_start_req_evt), sizeof(update_start_req_evt))){
         printf("Message %d sent \n",update_start_req_evt.header.frame_type);
     } else {
         printf("Failed to send message %d\n",update_start_req_evt.header.frame_type);
@@ -130,9 +134,9 @@ static void bm_dfu_host_send_chunk(bm_dfu_event_chunk_request_t* req) {
     printf("Processing chunk id %" PRIX32 "\n",req->seq_num);
     uint32_t payload_len = (host_ctx.bytes_remaining >= host_ctx.img_info.chunk_size) ? host_ctx.img_info.chunk_size : host_ctx.bytes_remaining;
     uint32_t payload_len_plus_header = sizeof(bcmp_dfu_payload_t) + payload_len;
-    uint8_t* buf = static_cast<uint8_t*>(pvPortMalloc(payload_len_plus_header));
-    configASSERT(buf);
-    bcmp_dfu_payload_t *payload_header = reinterpret_cast<bcmp_dfu_payload_t *>(buf);
+    uint8_t* buf = (uint8_t*)bm_malloc(payload_len_plus_header);
+    // configASSERT(buf);
+    bcmp_dfu_payload_t *payload_header = (bcmp_dfu_payload_t *)buf;
     payload_header->header.frame_type = BCMP_DFU_PAYLOAD;
     payload_header->chunk.addresses.src_node_id = host_ctx.self_node_id;
     payload_header->chunk.addresses.dst_node_id = host_ctx.client_node_id;
@@ -145,7 +149,7 @@ static void bm_dfu_host_send_chunk(bm_dfu_event_chunk_request_t* req) {
             bm_dfu_host_transition_to_error(BM_DFU_ERR_FLASH_ACCESS);
             break;
         }
-        if(host_ctx.bcmp_dfu_tx(static_cast<bcmp_message_type_t>(payload_header->header.frame_type), buf, payload_len_plus_header)){
+        if(host_ctx.bcmp_dfu_tx((bcmp_message_type_t)(payload_header->header.frame_type), buf, payload_len_plus_header)){
             host_ctx.bytes_remaining -= payload_len;
             printf("Message %d sent, payload size: %" PRIX32 ", remaining: %" PRIX32 "\n",payload_header->header.frame_type, payload_len, host_ctx.bytes_remaining);
         } else {
@@ -154,7 +158,7 @@ static void bm_dfu_host_send_chunk(bm_dfu_event_chunk_request_t* req) {
         }
     } while(0);
 
-    vPortFree(buf);
+    bm_free(buf);
 }
 
 /**
@@ -167,7 +171,7 @@ static void bm_dfu_host_send_reboot() {
     reboot_msg.addr.src_node_id = host_ctx.self_node_id;
     reboot_msg.addr.dst_node_id = host_ctx.client_node_id;
     reboot_msg.header.frame_type = BCMP_DFU_REBOOT;
-    if(host_ctx.bcmp_dfu_tx(static_cast<bcmp_message_type_t>(reboot_msg.header.frame_type), reinterpret_cast<uint8_t*>(&reboot_msg), sizeof(bcmp_dfu_reboot_t))){
+    if(host_ctx.bcmp_dfu_tx((bcmp_message_type_t)(reboot_msg.header.frame_type), (uint8_t*)(&reboot_msg), sizeof(bcmp_dfu_reboot_t))){
         printf("Message %d sent \n",reboot_msg.header.frame_type);
     } else {
         printf("Failed to send message %d\n",reboot_msg.header.frame_type);
@@ -189,8 +193,8 @@ void s_host_req_update_entry(void) {
         return;
     }
 
-    bm_dfu_frame_t *frame = reinterpret_cast<bm_dfu_frame_t *>(curr_evt.buf);
-    bm_dfu_event_img_info_t* img_info_evt = reinterpret_cast<bm_dfu_event_img_info_t*>(&(reinterpret_cast<uint8_t *>(frame))[1]);
+    bm_dfu_frame_t *frame = (bm_dfu_frame_t *)(curr_evt.buf);
+    bm_dfu_event_img_info_t* img_info_evt = (bm_dfu_event_img_info_t*)(&((uint8_t *)(frame))[1]);
     host_ctx.img_info = img_info_evt->img_info;
     host_ctx.bytes_remaining = host_ctx.img_info.image_size;
     host_ctx.client_node_id = img_info_evt->addresses.dst_node_id;
@@ -202,7 +206,8 @@ void s_host_req_update_entry(void) {
     bm_dfu_host_req_update();
 
     /* Kickoff ACK timeout */
-    configASSERT(xTimerStart(host_ctx.ack_timer, 10));
+    // configASSERT(xTimerStart(host_ctx.ack_timer, 10));
+    bm_timer_start(host_ctx.ack_timer, 10);
 }
 
 /**
@@ -218,15 +223,16 @@ void s_host_req_update_run(void)
 
     if (curr_evt.type == DFU_EVENT_ACK_RECEIVED) {
         /* Stop ACK Timer */
-        configASSERT(xTimerStop(host_ctx.ack_timer, 10));
-        configASSERT(curr_evt.buf);
-        bm_dfu_frame_t *frame = reinterpret_cast<bm_dfu_frame_t *>(curr_evt.buf);
-        bm_dfu_event_result_t* result_evt = reinterpret_cast<bm_dfu_event_result_t*>(&(reinterpret_cast<uint8_t *>(frame))[1]);
+        // configASSERT(xTimerStop(host_ctx.ack_timer, 10));
+        bm_timer_stop(host_ctx.ack_timer, 10);
+        // configASSERT(curr_evt.buf);
+        bm_dfu_frame_t *frame = (bm_dfu_frame_t *)(curr_evt.buf);
+        bm_dfu_event_result_t* result_evt = (bm_dfu_event_result_t*)(&((uint8_t *)(frame))[1]);
 
         if (result_evt->success) {
             bm_dfu_set_pending_state_change(BM_DFU_STATE_HOST_UPDATE);
         } else {
-            bm_dfu_host_transition_to_error(static_cast<bm_dfu_err_t>(result_evt->err_code));
+            bm_dfu_host_transition_to_error((bm_dfu_err_t)(result_evt->err_code));
         }
     } else if (curr_evt.type == DFU_EVENT_ACK_TIMEOUT) {
         host_ctx.ack_retry_num++;
@@ -236,14 +242,15 @@ void s_host_req_update_run(void)
             bm_dfu_host_transition_to_error(BM_DFU_ERR_TIMEOUT);
         } else {
             bm_dfu_host_req_update();
-            configASSERT(xTimerStart(host_ctx.ack_timer, 10));
+            // configASSERT(xTimerStart(host_ctx.ack_timer, 10));
+            bm_timer_start(host_ctx.ack_timer, 10);
         }
     } else if (curr_evt.type == DFU_EVENT_ABORT) {
         bm_dfu_err_t err = BM_DFU_ERR_ABORTED;
         if (curr_evt.buf)
         {
-            bcmp_dfu_abort_t* abort_evt = reinterpret_cast<bcmp_dfu_abort_t *>(curr_evt.buf);
-            err = static_cast<bm_dfu_err_t>(abort_evt->err.err_code);
+            bcmp_dfu_abort_t* abort_evt = (bcmp_dfu_abort_t *)(curr_evt.buf);
+            err = (bm_dfu_err_t)(abort_evt->err.err_code);
         }
         printf("Recieved abort in request.\n");
         bm_dfu_host_transition_to_error(err);
@@ -274,32 +281,35 @@ void s_host_update_run(void) {
 
     /* Check if we even have a buf to inspect */
     if (curr_evt.buf) {
-        frame = reinterpret_cast<bm_dfu_frame_t *>(curr_evt.buf);
+        frame = (bm_dfu_frame_t *)(curr_evt.buf);
     }
 
     if (curr_evt.type == DFU_EVENT_CHUNK_REQUEST) {
-        configASSERT(frame);
-        bm_dfu_event_chunk_request_t* chunk_req_evt = reinterpret_cast<bm_dfu_event_chunk_request_t*>(&(reinterpret_cast<uint8_t *>(frame))[1]);
+        // configASSERT(frame);
+        bm_dfu_event_chunk_request_t* chunk_req_evt = (bm_dfu_event_chunk_request_t*)(&((uint8_t *)(frame))[1]);
 
         /* Request Next Chunk */
         /* Send Heartbeat to Client */
-        configASSERT(xTimerStart(host_ctx.heartbeat_timer, 10));
+        // configASSERT(xTimerStart(host_ctx.heartbeat_timer, 10));
+        bm_timer_start(host_ctx.heartbeat_timer, 10);
 
         /* resend the frame to the client as is */
         bm_dfu_host_send_chunk(chunk_req_evt);
 
-        configASSERT(xTimerStop(host_ctx.heartbeat_timer, 10));
+        // configASSERT(xTimerStop(host_ctx.heartbeat_timer, 10));
+        bm_timer_stop(host_ctx.heartbeat_timer, 10);
     } else if (curr_evt.type == DFU_EVENT_REBOOT_REQUEST) {
-        configASSERT(frame);
+        // configASSERT(frame);
         bm_dfu_host_send_reboot();
     } else if (curr_evt.type == DFU_EVENT_BOOT_COMPLETE) {
-        configASSERT(frame);
+        // configASSERT(frame);
         bm_dfu_update_end(host_ctx.client_node_id, true, BM_DFU_ERR_NONE);
     }
     else if (curr_evt.type == DFU_EVENT_UPDATE_END) {
-        configASSERT(xTimerStop(host_ctx.update_timer, 100));
-        configASSERT(frame);
-        bm_dfu_event_result_t* update_end_evt = reinterpret_cast<bm_dfu_event_result_t*>(&(reinterpret_cast<uint8_t *>(frame))[1]);
+        // configASSERT(xTimerStop(host_ctx.update_timer, 100));
+        bm_timer_stop(host_ctx.update_timer, 100);
+        // configASSERT(frame);
+        bm_dfu_event_result_t* update_end_evt = (bm_dfu_event_result_t*)(&((uint8_t *)(frame))[1]);
 
         if (update_end_evt->success) {
             printf("Successfully updated Client\n");
@@ -307,15 +317,15 @@ void s_host_update_run(void) {
             printf("Client Update Failed\n");
         }
         if(host_ctx.update_complete_callback) {
-            host_ctx.update_complete_callback(update_end_evt->success, static_cast<bm_dfu_err_t>(update_end_evt->err_code), host_ctx.client_node_id);
+            host_ctx.update_complete_callback(update_end_evt->success, (bm_dfu_err_t)(update_end_evt->err_code), host_ctx.client_node_id);
         }
         bm_dfu_set_pending_state_change(BM_DFU_STATE_IDLE);
     } else if (curr_evt.type == DFU_EVENT_ABORT) {
         bm_dfu_err_t err = BM_DFU_ERR_ABORTED;
         if (curr_evt.buf)
         {
-            bcmp_dfu_abort_t* abort_evt = reinterpret_cast<bcmp_dfu_abort_t *>(curr_evt.buf);
-            err = static_cast<bm_dfu_err_t>(abort_evt->err.err_code);
+            bcmp_dfu_abort_t* abort_evt = (bcmp_dfu_abort_t *)(curr_evt.buf);
+            err = (bm_dfu_err_t)(abort_evt->err.err_code);
         }
         printf("Recieved abort in run.\n");
         bm_dfu_host_transition_to_error(err);
@@ -323,27 +333,27 @@ void s_host_update_run(void) {
 }
 
 void bm_dfu_host_init(bcmp_dfu_tx_func_t bcmp_dfu_tx) {
-    configASSERT(bcmp_dfu_tx);
+    // configASSERT(bcmp_dfu_tx);
     host_ctx.bcmp_dfu_tx = bcmp_dfu_tx;
     int tmr_id = 0;
 
     /* Store relevant variables */
-    host_ctx.self_node_id = getNodeId();
+    host_ctx.self_node_id = node_id();
 
     /* Get DFU Subsystem Queue */
     host_ctx.dfu_event_queue = bm_dfu_get_event_queue();
 
     /* Initialize ACK and Heartbeat Timer */
-    host_ctx.ack_timer = xTimerCreate("DFU Host Ack", (BM_DFU_HOST_ACK_TIMEOUT_MS / portTICK_RATE_MS),
-                                      pdFALSE, (void *) &tmr_id, ack_timer_handler);
-    configASSERT(host_ctx.ack_timer);
+    host_ctx.ack_timer = bm_timer_create("DFU Host Ack", bm_ms_to_ticks(BM_DFU_HOST_ACK_TIMEOUT_MS),
+                                      false, (void *) &tmr_id, ack_timer_handler);
+    // configASSERT(host_ctx.ack_timer);
 
-    host_ctx.heartbeat_timer = xTimerCreate("DFU Host Heartbeat", (BM_DFU_HOST_HEARTBEAT_TIMEOUT_MS / portTICK_RATE_MS),
-                                      pdTRUE, (void *) &tmr_id, heartbeat_timer_handler);
-    configASSERT(host_ctx.heartbeat_timer);
-    host_ctx.update_timer = xTimerCreate("update timer", pdMS_TO_TICKS(BM_DFU_UPDATE_DEFAULT_TIMEOUT_MS),
-                                    pdFALSE, (void *) &tmr_id, update_timer_handler);
-    configASSERT(host_ctx.update_timer);
+    host_ctx.heartbeat_timer = bm_timer_create("DFU Host Heartbeat", bm_ms_to_ticks(BM_DFU_HOST_HEARTBEAT_TIMEOUT_MS),
+                                      true, (void *) &tmr_id, heartbeat_timer_handler);
+    // configASSERT(host_ctx.heartbeat_timer);
+    host_ctx.update_timer = bm_timer_create("update timer", bm_ms_to_ticks(BM_DFU_UPDATE_DEFAULT_TIMEOUT_MS),
+                                    false, (void *) &tmr_id, update_timer_handler);
+    // configASSERT(host_ctx.update_timer);
 }
 
 void bm_dfu_host_set_params(update_finish_cb_t update_complete_callback, uint32_t hostTimeoutMs) {
@@ -352,15 +362,21 @@ void bm_dfu_host_set_params(update_finish_cb_t update_complete_callback, uint32_
 }
 
 static void bm_dfu_host_start_update_timer(uint32_t timeoutMs) {
-    configASSERT(xTimerStop(host_ctx.update_timer, 100));
-    configASSERT(xTimerChangePeriod(host_ctx.update_timer, pdMS_TO_TICKS(timeoutMs), 100));
-    configASSERT(xTimerStart(host_ctx.update_timer, 100));
+    // configASSERT(xTimerStop(host_ctx.update_timer, 100));
+    // configASSERT(xTimerChangePeriod(host_ctx.update_timer, bm_ms_to_ticks(timeoutMs), 100));
+    // configASSERT(xTimerStart(host_ctx.update_timer, 100));
+    bm_timer_stop(host_ctx.update_timer, 100);
+    bm_timer_change_period(host_ctx.update_timer, bm_ms_to_ticks(timeoutMs), 100);
+    bm_timer_start(host_ctx.update_timer, 100);
 }
 
 static void bm_dfu_host_transition_to_error(bm_dfu_err_t err) {
-    configASSERT(xTimerStop(host_ctx.update_timer, 100));
-    configASSERT(xTimerStop(host_ctx.heartbeat_timer, 10));
-    configASSERT(xTimerStop(host_ctx.ack_timer, 10));
+    // configASSERT(xTimerStop(host_ctx.update_timer, 100));
+    // configASSERT(xTimerStop(host_ctx.heartbeat_timer, 10));
+    // configASSERT(xTimerStop(host_ctx.ack_timer, 10));
+    bm_timer_stop(host_ctx.update_timer, 100);
+    bm_timer_stop(host_ctx.heartbeat_timer, 10);
+    bm_timer_stop(host_ctx.ack_timer, 10);
     bm_dfu_set_error(err);
     bm_dfu_set_pending_state_change(BM_DFU_STATE_ERROR);
 }
