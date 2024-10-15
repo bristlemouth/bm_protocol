f2d7ca304cd0118bb835fc1b69ea39cd460ad5d7
Matthew Krause <kraus2mj@gmail.com>
2024-10-15 15:03:24 -0700

    Abstracts LWIP API From Bristlemouth Middleware
    Adds further API to bm_ip.h in order to remove lwip dependencies

--- src/lib/middleware/middleware.h
+++ src/lib/middleware/middleware.h
@@ -2,16 +2,19 @@
 
 #include <stdbool.h>
 #include <stdint.h>
-#include "lwip/netif.h"
-#include "lwip/pbuf.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+#include "util.h"
 
-int32_t bm_middleware_local_pub(struct pbuf *pbuf);
-void bm_middleware_init(struct netif* netif, uint16_t port);
-int32_t middleware_net_tx(struct pbuf *pbuf);
+#ifndef middleware_net_task_priority
+#define middleware_net_task_priority 4
+#endif
+
+BmErr bm_middleware_local_pub(void *buf, uint32_t size);
+BmErr bm_middleware_init(uint16_t port);
+BmErr bm_middleware_net_tx(void *buf, uint32_t size);
 
 #ifdef __cplusplus
 }
