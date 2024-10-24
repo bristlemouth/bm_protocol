c114ebe527e0c7c717ddff8b4c0a5de734fdce5d
Matthew Krause <61891357+matt001k@users.noreply.github.com>
2024-10-24 19:41:50 -0400

    Supported services port (#196)
    Ports Supported Services To Utilize bm_core API
    
    Refactor Common Messages For Services To Be In C
    This changes the CMakeLists files to include these c files
    Changes reference to bm_common_messages submodule
    Updates API for these re-factored functions
    Add Global Pointer To HW Configuration Class
    Use this in the wrapper file for bm_configs
    Changes Pass By Reference C++ With C Pointer Reference In bm_service

--- src/lib/middleware/bm_service.h
+++ src/lib/middleware/bm_service.h
@@ -18,11 +18,11 @@
  * @param[out] reply_data The reply data buffer to be filled in.
  * @return True if the request was handled, false otherwise.
  */
-typedef bool (*bm_service_handler)(size_t service_strlen, 
-                                    const char * service, 
-                                    size_t req_data_len, 
+typedef bool (*bm_service_handler)(size_t service_strlen,
+                                    const char * service,
+                                    size_t req_data_len,
                                     uint8_t * req_data,
-                                    size_t &buffer_len,
+                                    size_t *buffer_len,
                                     uint8_t * reply_data);
 
 void bm_service_init(void);
