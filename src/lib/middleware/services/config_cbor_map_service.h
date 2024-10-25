7eacf2413c9d2df96d70f729e13bd7feaa7d4e14
Victor Sowa <victor.sowa@sofarocean.com>
2024-10-25 11:37:45 -0700

    couple more merge conflict changes and point bm_common_messages to that repo's experiment/bm_core branch

--- src/lib/middleware/services/config_cbor_map_service.h
+++ src/lib/middleware/services/config_cbor_map_service.h
@@ -10,4 +10,4 @@ constexpr uint32_t CONFIG_CBOR_MAP_PARTITION_ID_USER = 3;
 
 void config_cbor_map_service_init(void);
 bool config_cbor_map_service_request(uint64_t target_node_id, uint32_t partition_id,
-                                     bm_service_reply_cb reply_cb, uint32_t timeout_s);
+                                     BmServiceReplyCb reply_cb, uint32_t timeout_s);
--- src/lib/middleware/services/sys_info_service.h
+++ src/lib/middleware/services/sys_info_service.h
@@ -5,5 +5,5 @@ extern "C" {
 }
 
 void sys_info_service_init(void);
-bool sys_info_service_request(uint64_t target_node_id, bm_service_reply_cb reply_cb,
+bool sys_info_service_request(uint64_t target_node_id, BmServiceReplyCb reply_cb,
                               uint32_t timeout_s);
