53af8d5360d0c7b284cfb70d9a122647f8efa9ee
evanShap <evan@sofarocean.com>
2024-10-25 21:57:22 -0700

    reimplement OrderedSeparatorLineParser parsing to work on multi-character sets for separators, pass the separator to the value parsing function

--- src/lib/common/OrderedSeparatorLineParser.cpp
+++ src/lib/common/OrderedSeparatorLineParser.cpp
@@ -4,36 +4,47 @@
 
 #include "OrderedSeparatorLineParser.h"
 #include "string.h"
-#include <cstdlib>
 
 OrderedSeparatorLineParser::OrderedSeparatorLineParser(const char* separator, size_t maxLineLen,
                                                        const ValueType* valueTypes, size_t numValues, const char* header /*= nullptr*/)
     : LineParser(separator, maxLineLen, valueTypes, numValues, header) { }
 
-// Parse the value based on its type
-// workStr is a copy of the line - note strtok_r will modify *workStr in-place
 bool OrderedSeparatorLineParser::parseValues(char* workStr) {
-  char* saveptr; // Additional pointer for strtok_r so it stays thread-safe
-  // Parse number of expected values from the string
-  for (size_t i = 0; i < _numValues; i++) {
-    // First call to strok_r, workStr will be non-NULL (start of string).
-    //    strtok_r will set first occurence of _separator in workStr to NULL
-    //    set saveptr to the next byte
-    //    then return workStr
-    // Subsequent calls workStr is NULL, telling strtok_r to start search from saveptr
+  const char* tokenStart = workStr;
+  char valueSeparator = '\0';
 
-    char* token = strtok_r(workStr, _separator, &saveptr);
-    if (token == nullptr) {
-      printf("ERR - expected next separator: \"%c\" not found in line!\n", _separator[0]);
-      return false; // No separator found
-    }
-    // Parse the value at specified index
-    if (!parseValueFromToken(token, i)) {
+  for (size_t i = 0; i < _numValues; i++) {
+    if (tokenStart == nullptr) {
+      printf("ERR - no token to parse from!\n");
       return false;
     }
-    // Prepare for the next strtok_r call
-    workStr = nullptr;
+    // Find the first occurrence of any separator character. strpbrk looks for the first
+    //  occurence of any of the target characters in the set of characters in the _separator
+    //  string. It does not match multi-character patterns.
+    char* sepPos = strpbrk(tokenStart, _separator);
+    // Some parsers need to interpret the separator, so we'll save the detected parser for them.
+    char nextSeparator = '\0';
+    if (sepPos) {
+      nextSeparator = *sepPos;
+      *sepPos = '\0'; // Split the string at the separator for parsing.
+    } else if (i < _numValues-1) {
+      // Error if we don't find the next separator and we're not parsing the last value.
+      printf("ERR - expected next separator: \"%s\" not found in line!\n", _separator);
+      return false;
+    }
+    // Parse the value at the current position and pass along the previously saved separator
+    if (!parseValueFromToken(tokenStart, i, valueSeparator)) {
+      return false;
+    }
+    // Save the next separator in outer scope so can use when parsing next value
+    if (sepPos != nullptr) {
+      valueSeparator = nextSeparator;
+      // Move to the next part of the string after the separator
+      tokenStart = sepPos + 1;
+    } else {
+      valueSeparator = '\0';
+      tokenStart = nullptr;
+    }
   }
-
   return true;
 }
