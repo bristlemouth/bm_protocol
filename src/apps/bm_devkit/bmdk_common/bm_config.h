69415b45afc36e6ba4aa9a8f731905229bbae33b
Matthew Krause <kraus2mj@gmail.com>
2024-10-29 12:35:20 -0700

    Adds bm_config Template/Documentation For Integrators
    Updates printf statments with bm_debug
    Adds in bm_app_name macro user must define

--- src/apps/bm_devkit/bmdk_common/bm_config.h
+++ src/apps/bm_devkit/bmdk_common/bm_config.h
@@ -1,26 +1,14 @@
 #ifndef __BM_CONFIG_H__
 #define __BM_CONFIG_H__
 
-#include "adin2111.h"
-#include <stdbool.h>
-#include <stdint.h>
-#include <string.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif /* __cplusplus */
+#include <stdlib.h>
 
 #define ADIN_PORT_MASK_1 (1 << ADIN2111_PORT_1)
 #define ADIN_PORT_MASK_2 (1 << ADIN2111_PORT_2)
 #define ADIN_PORT_MASK_ALL (ADIN_PORT_MASK_1 | ADIN_PORT_MASK_2)
 
-typedef struct adin2111_config_s {
-  uint32_t port_mask;
-  adin2111_DeviceHandle_t dev;
-} adin2111_config_t;
+#define bm_app_name APP_NAME
 
-#ifdef __cplusplus
-}
-#endif
+#define bm_debug(format, ...) printf(format, ##__VA_ARGS__)
 
 #endif /* __BM_CONFIG_H__ */
