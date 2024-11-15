b6851f3a48f1d11d92b928d2597d96ebfe4d06c4
Matthew Krause <61891357+matt001k@users.noreply.github.com>
2024-11-15 15:48:21 -0800

    Update Pub/Sub Return Values Checking (#210)
    * Update Pub/Sub Return Values Checking
    
    * Update submodule reference for bm_core

--- src/lib/bm_ncp/ncp_uart.cpp
+++ src/lib/bm_ncp/ncp_uart.cpp
@@ -91,15 +91,15 @@ static bool bm_serial_pub_cb(const char *topic, uint16_t topic_len, uint64_t nod
                              uint8_t version) {
   printf("Pub data on topic \"%.*s\" from %016" PRIx64 " Type: %u, Version: %u\n", topic_len,
          topic, node_id, type, version);
-  return bm_pub_wl(topic, topic_len, payload, len, type, version);
+  return bm_pub_wl(topic, topic_len, payload, len, type, version) == BmOK;
 }
 
 static bool bm_serial_sub_cb(const char *topic, uint16_t topic_len) {
-  return bm_sub_wl(topic, topic_len, ncp_uart_pub_cb);
+  return bm_sub_wl(topic, topic_len, ncp_uart_pub_cb) == BmOK;
 }
 
 static bool bm_serial_unsub_cb(const char *topic, uint16_t topic_len) {
-  return bm_unsub_wl(topic, topic_len, ncp_uart_pub_cb);
+  return bm_unsub_wl(topic, topic_len, ncp_uart_pub_cb) == BmOK;
 }
 
 static bool ncp_log_cb(uint64_t node_id, const uint8_t *data, size_t len) {
