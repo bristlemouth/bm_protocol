da5c128c74f95216ae0f82e1aca7b52eac28be5d
Matthew Krause <61891357+matt001k@users.noreply.github.com>
2024-12-06 13:18:25 -0800

    Update Bridge To Inform Spotter Of Its Hardware Revision (#219)
    * Update Bridge To Inform Spotter Of Its Hardware Revision
    
    Allows bridge to inform the spotter of its hardware revision with updated serial messages

--- src/lib/common/device_info.h
+++ src/lib/common/device_info.h
@@ -1,27 +1,29 @@
 #pragma once
 
+#include "version.h"
 #include <stdbool.h>
 #include <stddef.h>
-#include "version.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-const versionInfo_t *getVersionInfo();
+const versionInfo_t *getVersionInfo(void);
 const versionInfo_t *findVersionInfo(uint32_t addr, uint32_t len);
 
 bool fwIsEng(const versionInfo_t *info);
 bool fwIsDirty(const versionInfo_t *info);
-const uint32_t* getUID();
-const char * getUIDStr();
-const char * getFWVersionStr();
-uint32_t getGitSHA();
+const uint32_t *getUID(void);
+const char *getUIDStr(void);
+const char *getFWVersionStr(void);
+uint32_t getGitSHA(void);
 void getFWVersion(uint8_t *major, uint8_t *minor, uint8_t *revision);
 size_t getBuildId(const uint8_t **buildId);
 void getMacAddr(uint8_t *buff, size_t len);
-uint64_t getNodeId();
-const char * getNodeIdStr();
+uint64_t getNodeId(void);
+const char *getNodeIdStr(void);
+void setHwVersion(uint8_t version);
+uint8_t getHwVersion(void);
 #ifdef __cplusplus
 }
 #endif
