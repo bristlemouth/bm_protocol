ff876e0e4e9d45f7edf372dc9c64eb520690c1c8
Victor Sowa <victor.sowa@sofarocean.com>
2025-01-03 16:31:30 -0800

    make sure we compare to BmOK

--- src/apps/bridge/sensor_drivers/pmeDissolvedOxygenSensor.cpp
+++ src/apps/bridge/sensor_drivers/pmeDissolvedOxygenSensor.cpp
@@ -26,7 +26,7 @@ bool PmeDissolvedOxygenSensor::subscribe() {
     int topic_strlen =
         snprintf(sub, BM_TOPIC_MAX_LEN, "sensor/%016" PRIx64 "%s", node_id, subtag);
     if (topic_strlen > 0) {
-      rval = bm_sub_wl(sub, topic_strlen, pmeDissolvedOxygenSubCallback);
+      rval = (bm_sub_wl(sub, topic_strlen, pmeDissolvedOxygenSubCallback) == BmOK);
     }
     bm_free(sub);
   }
