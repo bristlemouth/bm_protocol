cd5ce6784e0433c60d8ecba07543183300694e6d
Matthew Krause <kraus2mj@gmail.com>
2025-01-15 15:07:49 -0800

    Adds Port Monitor Module To Turn Off Unused Ports
    Port monitor will disable ports if there has been no communication since boot
    Adds configurations to enable this feature and how long the node will be
    on until ports are checked to turn them off

---
+++ src/lib/common/port_monitor.c
@@ -0,0 +1,93 @@
+#include "port_monitor.h"
+#include "bm_config.h"
+#include "bm_os.h"
+#include "configuration.h"
+#include "l2.h"
+#include <string.h>
+
+#define port_monitor_time_ms_key "disableUnusedPortsTimeMs"
+#define port_monitor_time_ms_default (0U)
+
+static uint16_t ENABLED_PORT_MASK = 0;
+
+/*!
+  @brief Link change callback to set ports which have come online
+
+  @details Will set a masked value of the ports which have come online.
+
+  @param port port that has had a link change
+  @param state state of the port
+ */
+static void port_monitor_link_change(uint8_t port, bool state) {
+  // If the port is online, update the mask
+  if (state) {
+    ENABLED_PORT_MASK |= 1 << (port - 1);
+  }
+}
+
+/*!
+  @brief Timer callback handler to determine if all ports have come online
+
+  @details Checks if all ports on the node have come online, if a port is
+           not online, disable the unused port.
+ 
+  @param timer unused
+ */
+static void port_monitor_timer_handler(BmTimer timer) {
+  (void)timer;
+  uint8_t ports = bm_l2_get_port_count();
+
+  for (uint8_t i = 0; i < ports; i++) {
+    uint16_t enabled = (1 << i) & ENABLED_PORT_MASK;
+    uint8_t port_num = i + 1;
+    if (enabled == 0) {
+      bm_debug("Disabling port %d, it never came online\n", port_num);
+      bm_l2_netif_enable_disable_port(port_num, false);
+    }
+  }
+}
+
+/*!
+  @brief Obtain configuration for port monitor module
+
+  @details If configuration does not exist, set it to the default
+           value and save.
+
+  @param ms pointer to variable which will hold the configuration value
+            for port monitor
+ */
+static inline void port_monitor_get_configs(uint32_t *ms) {
+  if (!get_config_uint(BM_CFG_PARTITION_SYSTEM, port_monitor_time_ms_key,
+                       strlen(port_monitor_time_ms_key), ms)) {
+    set_config_uint(BM_CFG_PARTITION_SYSTEM, port_monitor_time_ms_key,
+                    strlen(port_monitor_time_ms_key), port_monitor_time_ms_default);
+    save_config(BM_CFG_PARTITION_SYSTEM, false);
+  }
+}
+
+/*!
+ @brief Initialize port monitor
+
+ @details The port monitor will turn off unused ports on the device. A
+          configuration value is used to turn on the port monitor and
+          determine the time necessary before turning off that port.
+
+ @return BmOK on success or if not configured
+ @return BmErr on failure
+ */
+BmErr port_monitor_init(void) {
+  BmErr err = BmOK;
+  uint32_t ms = port_monitor_time_ms_default;
+  BmTimer port_monitor_timer = NULL;
+
+  port_monitor_get_configs(&ms);
+
+  if (ms) {
+    bm_l2_register_link_change_callback(port_monitor_link_change);
+    port_monitor_timer =
+        bm_timer_create("port_monitor_timer", ms, false, NULL, port_monitor_timer_handler);
+    err = bm_timer_start(port_monitor_timer, 0);
+  }
+
+  return err;
+}
