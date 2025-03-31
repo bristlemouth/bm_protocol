3496135d500d7cc7919fd0fc73f5ec4af5be1df4
Matthew Krause <kraus2mj@gmail.com>
2025-03-31 14:09:07 -0700

    Wrap calloc too just in case

--- src/lib/common/malloc.c
+++ src/lib/common/malloc.c
@@ -1,7 +1,18 @@
 #include "bm_os.h"
 #include <stdio.h>
+#include <string.h>
 
 void *__wrap_malloc(size_t size) { return bm_malloc(size); }
+
+void *__wrap_calloc(size_t num, size_t size) {
+  void *ret = bm_malloc(num * size);
+
+  if (ret) {
+    memset(ret, 0, num * size);
+  }
+
+  return ret;
+}
 
 void __wrap_free(void *p) {
   if (p) {
