23dc2e490ab2cb08b995d3e6d9d1c378ffbf2456
Michael Bella <michael.bella@sofarocean.com>
2025-07-02 13:22:53 -0700

    Fixing issues

--- src/apps/bm_devkit/arpa-e_example/user_code/user_code.cpp
+++ src/apps/bm_devkit/arpa-e_example/user_code/user_code.cpp
@@ -1,12 +1,14 @@
 #include "user_code.h"
 #include "bq25820.h"
 #include "ina232.h"
+#include "serial.h"
+#include "bsp.h"
 
 static BQ::BQ25820 Charger(&i2c1);
 
 void setup(void) {
   // Turn on the VBUS load switch on the mote
-  IOWrite(&VBUS_EN, 0);
+  IOWrite(&VBUS_BF_EN, 0);
   // Wait before trying to initialize the battery charger IC
   vTaskDelay(pdMS_TO_TICKS(20));
   // Try to initialize the charger IC
