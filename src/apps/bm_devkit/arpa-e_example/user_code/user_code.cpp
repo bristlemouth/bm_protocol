9d6d509b92bd0af56592b4d7ec1da9583df4dfd0
Michael Bella <michael.bella@sofarocean.com>
2025-07-02 16:32:06 -0700

    Fixed mixed spaces and tabs in user_code.cpp

--- src/apps/bm_devkit/arpa-e_example/user_code/user_code.cpp
+++ src/apps/bm_devkit/arpa-e_example/user_code/user_code.cpp
@@ -1,8 +1,8 @@
 #include "user_code.h"
-#include "bsp.h"
 #include "bq25820.h"
-#include "ina232.h"
+#include "bsp.h"
 #include "debug.h"
+#include "ina232.h"
 
 static BQ::BQ25820 Charger(&i2c1);
 
@@ -12,16 +12,14 @@ void setup(void) {
   // Wait before trying to initialize the battery charger IC
   vTaskDelay(pdMS_TO_TICKS(20));
   // Try to initialize the charger IC
-  if(Charger.init()) {
-      printf("Charger IC Initialized\n");
-			printf("Disabling pulse frequency modulation on the charger.\n");
-      Charger.disablePfm();
-  }
-  else {
-      printf("Failed to init the charger IC.\n");
+  if (Charger.init()) {
+    printf("Charger IC Initialized\n");
+    printf("Disabling pulse frequency modulation on the charger.\n");
+    Charger.disablePfm();
+  } else {
+    printf("Failed to init the charger IC.\n");
   }
   /* USER ONE-TIME SETUP CODE GOES HERE */
-
 }
 
 void loop(void) { /* USER LOOP CODE GOES HERE */ }
