a65654dc8e26efbf00c1b47a8ae7dc1f697f2959
Matthew Krause <kraus2mj@gmail.com>
2025-07-02 08:34:21 -0700

    Remove un-necessary MACRO

--- src/apps/bridge/topology_sampler.cpp
+++ src/apps/bridge/topology_sampler.cpp
@@ -46,7 +46,6 @@
 #define NODE_CONFIG_CBOR_MAP_REQUEST_TIMEOUT_MS (NODE_CONFIG_CBOR_MAP_REQUEST_TIMEOUT_S * 1000)
 // Accounts for name of app + cbor config map + encoding inefficiencies
 #define NODE_CONFIG_PADDING (512)
-#define NETWORK_TOPOLOGY_QUEUE_SIZE (1)
 
 typedef struct network_configuration_info {
   uint32_t network_crc32;
