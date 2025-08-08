db2dd6ab5184b09fb6f9ebbbe5097075bdda8a20
Victor Sowa <victor.sowa@sofarocean.com>
2025-08-08 15:49:07 -0700

    this didn't work

--- src/lib/bm_ncp/ncp_uart.cpp
+++ src/lib/bm_ncp/ncp_uart.cpp
@@ -357,7 +357,6 @@ static bool bm_int_gpio_callback_fromISR(const void *pinHandle, uint8_t value, v
 
   if (value) {
     xSemaphoreGiveFromISR(ncp_serial_lock, &xHigherPriorityTaskWoken);
-    HAL_UART_DMAStop(&huart3);
     ncp_rx = false;
   } else {
     xSemaphoreTakeFromISR(ncp_serial_lock, &xHigherPriorityTaskWoken);
