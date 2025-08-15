111c528060349ab60c045a57f4a21c3e857aa2fd
Victor Sowa <victor.sowa@sofarocean.com>
2025-08-15 16:01:59 -0700

    update the bsp and remove usart3 dma + switch usart3 back to LL drivers, not HAL drivers

--- src/bsp/bridge_v1_0/Core/Inc/stm32u5xx_it.h
+++ src/bsp/bridge_v1_0/Core/Inc/stm32u5xx_it.h
@@ -22,7 +22,7 @@
 #define __STM32U5xx_IT_H
 
 #ifdef __cplusplus
- extern "C" {
+extern "C" {
 #endif
 
 /* Private includes ----------------------------------------------------------*/
@@ -52,8 +52,6 @@ void EXTI1_IRQHandler(void);
 void EXTI8_IRQHandler(void);
 void TIM8_UP_IRQHandler(void);
 void SPI2_IRQHandler(void);
-void GPDMA1_Channel10_IRQHandler(void);
-void GPDMA1_Channel11_IRQHandler(void);
 void GPDMA1_Channel12_IRQHandler(void);
 void GPDMA1_Channel13_IRQHandler(void);
 void GPDMA1_Channel14_IRQHandler(void);
