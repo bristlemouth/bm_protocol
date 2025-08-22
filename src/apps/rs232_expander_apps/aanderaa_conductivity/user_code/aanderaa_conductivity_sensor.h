fbd021dfa7d28bd8b31117ab179bf903209fe0aa
uma-kat <84096974+uma-kat@users.noreply.github.com>
2025-08-22 14:44:25 -0700

    presets

--- src/apps/rs232_expander_apps/aanderaa_conductivity/user_code/aanderaa_conductivity_sensor.h
+++ src/apps/rs232_expander_apps/aanderaa_conductivity/user_code/aanderaa_conductivity_sensor.h
@@ -21,21 +21,11 @@ private:
     static constexpr uint32_t BAUD_RATE = 9600;
     static constexpr char LINE_TERM = '\n';
     static constexpr ValueType PARSER_VALUE_TYPE[] = {
-        TYPE_DOUBLE, TYPE_DOUBLE, TYPE_DOUBLE, TYPE_DOUBLE, TYPE_DOUBLE, TYPE_FLOAT};
+        TYPE_DOUBLE, TYPE_DOUBLE, TYPE_DOUBLE, TYPE_DOUBLE, TYPE_DOUBLE, TYPE_DOUBLE};
     static constexpr char SENSOR_BM_LOG_ENABLE[] = "sensorBmLogEnable";
 
 private:
     uint32_t _sensorBmLogEnable = 0;
     OrderedSeparatorLineParser _parser;
     char _payload_buffer[2048];
-    /*
-    AanderaaConductivityMsg::Data _data; // Store the last read data
-    bool _data_available = false; // Flag to indicate if data is available
-    bool _data_parsed = false; // Flag to indicate if data has been parsed
-    bool _data_valid = false; // Flag to indicate if the parsed data is valid
-    bool _data_sent = false; // Flag to indicate if the data has been sent
-    bool _data_error = false; // Flag to indicate if there was an error in data
-    bool _data_flush = false; // Flag to indicate if the data should be flushed
-    bool _data_init = false; // Flag to indicate if the sensor has been initialized
-    */
 };
