3936559422b897593f9df8234681220c55307bdd
Ben Hagan <20977020+LarkspurRaven@users.noreply.github.com>
2025-09-17 12:16:54 -0700

    [CONDUCTIVITY_SNSR][TEST] Remove tests no longer needed

--- test/src/common/lineParsers_ut.cpp
+++ test/src/common/lineParsers_ut.cpp
@@ -16,7 +16,6 @@ extern "C" {
 DEFINE_FFF_GLOBALS;
 
 static constexpr ValueType rbrParserExample[3] = {TYPE_UINT64, TYPE_DOUBLE, TYPE_DOUBLE};
-static constexpr ValueType aanderaaConductivityParserExample[6] = {TYPE_DOUBLE, TYPE_DOUBLE, TYPE_DOUBLE, TYPE_DOUBLE, TYPE_DOUBLE, TYPE_DOUBLE};
 
 // The fixture for testing class Foo.
 class LineParserTest : public ::testing::Test {
@@ -28,18 +27,15 @@ protected:
   Exo3DataLineParser exoD0Parser;
   Exo3DataLineParser exoD2Parser;
   OrderedSeparatorLineParser rbrTestParser;
-  OrderedSeparatorLineParser aanderaaConductivityTestParser;
 
   LineParserTest()
       : exoD0Parser(4, "0D0!0"),
         exoD2Parser(2, "0D2!0"),
-         rbrTestParser(",", 256, rbrParserExample, 3),
-         aanderaaConductivityTestParser(",", 256, aanderaaConductivityParserExample, 6) {
+         rbrTestParser(",", 256, rbrParserExample, 3) {
     // You can do set-up work for each test here if needed.
     exoD0Parser.init();
     exoD2Parser.init();
     rbrTestParser.init();
-    aanderaaConductivityTestParser.init();
   }
 
   ~LineParserTest() override {
@@ -204,97 +200,5 @@ TEST_F(LineParserTest, ExoD0Parser_FailBadSeparator) {
 TEST_F(LineParserTest, ExoD0Parser_FailWrongResponseHeader) {
   const char* test_line = "0D1!0+25.044,7.13+6.92-9.74";
   bool success = exoD0Parser.parseLine(test_line, strlen(test_line));
-  EXPECT_EQ(success, false);
-}
-
-// Parse a single good line of Aanderaa Conductivity data
-TEST_F(LineParserTest, AanderaaConductivityParser_Valid) {
-  const char* test_line = "50.123,23.456,35.123,1025.83,1498.15,10.0";
-  bool success = aanderaaConductivityTestParser.parseLine(test_line, strlen(test_line));
-  EXPECT_EQ(success, true);
-
-  // Retrieve and validate each value
-  Value conductivity = aanderaaConductivityTestParser.getValue(0);
-  EXPECT_EQ(conductivity.type, TYPE_DOUBLE);
-  EXPECT_NEAR(conductivity.data.double_val, 50.123, 1e-6);
-
-  Value temperature = aanderaaConductivityTestParser.getValue(1);
-  EXPECT_EQ(temperature.type, TYPE_DOUBLE);
-  EXPECT_NEAR(temperature.data.double_val, 23.456, 1e-6);
-
-  Value salinity = aanderaaConductivityTestParser.getValue(2);
-  EXPECT_EQ(salinity.type, TYPE_DOUBLE);
-  EXPECT_NEAR(salinity.data.double_val, 35.123, 1e-6);
-
-  Value water_density = aanderaaConductivityTestParser.getValue(3);
-  EXPECT_EQ(water_density.type, TYPE_DOUBLE);
-  EXPECT_NEAR(water_density.data.double_val, 1025.83, 1e-6);
-
-  Value sound_speed = aanderaaConductivityTestParser.getValue(4);
-  EXPECT_EQ(sound_speed.type, TYPE_DOUBLE);
-  EXPECT_NEAR(sound_speed.data.double_val, 1498.15, 1e-6);
-
-  Value depth = aanderaaConductivityTestParser.getValue(5);
-  EXPECT_EQ(depth.type, TYPE_DOUBLE);
-  EXPECT_NEAR(depth.data.double_val, 10.0, 1e-6);
-}
-
-TEST_F(LineParserTest, AanderaaConductivityParser_ValidWithNegativeTemp) {
-  const char* test_line = "45.678,-2.345,30.567,1020.45,1485.67,5.5";
-  bool success = aanderaaConductivityTestParser.parseLine(test_line, strlen(test_line));
-  EXPECT_EQ(success, true);
-
-  Value temperature = aanderaaConductivityTestParser.getValue(1);
-  EXPECT_EQ(temperature.type, TYPE_DOUBLE);
-  EXPECT_NEAR(temperature.data.double_val, -2.345, 1e-6);
-}
-
-TEST_F(LineParserTest, AanderaaConductivityParser_ValidBoundaryValues) {
-  const char* test_line = "0.0,-5.0,0.0,990.0,1400.0,0.0";
-  bool success = aanderaaConductivityTestParser.parseLine(test_line, strlen(test_line));
-  EXPECT_EQ(success, true);
-
-  Value conductivity = aanderaaConductivityTestParser.getValue(0);
-  EXPECT_NEAR(conductivity.data.double_val, 0.0, 1e-6);
-
-  Value temperature = aanderaaConductivityTestParser.getValue(1);
-  EXPECT_NEAR(temperature.data.double_val, -5.0, 1e-6);
-}
-
-TEST_F(LineParserTest, AanderaaConductivityParser_FailBadFirstField) {
-  const char* test_line = "a50.123,23.456,35.123,1025.83,1498.15,10.0";
-  bool success = aanderaaConductivityTestParser.parseLine(test_line, strlen(test_line));
-  EXPECT_EQ(success, false);
-}
-
-TEST_F(LineParserTest, AanderaaConductivityParser_FailTooFewFields) {
-  const char* test_line = "50.123,23.456,35.123";
-  bool success = aanderaaConductivityTestParser.parseLine(test_line, strlen(test_line));
-  EXPECT_EQ(success, false);
-}
-
-TEST_F(LineParserTest, AanderaaConductivityParser_IgnoreExtraFields) {
-  // OrderedSeparatorLineParser ignores extra fields by design - this should succeed
-  const char* test_line = "50.123,23.456,35.123,1025.83,1498.15,10.0,extra";
-  bool success = aanderaaConductivityTestParser.parseLine(test_line, strlen(test_line));
-  EXPECT_EQ(success, true);
-
-  // Verify the first 6 fields were parsed correctly
-  Value conductivity = aanderaaConductivityTestParser.getValue(0);
-  EXPECT_NEAR(conductivity.data.double_val, 50.123, 1e-6);
-
-  Value depth = aanderaaConductivityTestParser.getValue(5);
-  EXPECT_NEAR(depth.data.double_val, 10.0, 1e-6);
-}
-
-TEST_F(LineParserTest, AanderaaConductivityParser_FailInvalidFloat) {
-  const char* test_line = "50.123,23.456,35.123,1025.83,1498.15,abc";
-  bool success = aanderaaConductivityTestParser.parseLine(test_line, strlen(test_line));
-  EXPECT_EQ(success, false);
-}
-
-TEST_F(LineParserTest, AanderaaConductivityParser_FailEmptyField) {
-  const char* test_line = "50.123,,35.123,1025.83,1498.15,10.0";
-  bool success = aanderaaConductivityTestParser.parseLine(test_line, strlen(test_line));
   EXPECT_EQ(success, false);
 }
