67ce916fcb641a2ecdef32375ed68c29d8ef3bcd
Matthew Krause <kraus2mj@gmail.com>
2025-11-05 16:53:12 -0800

    Update spotter log aggregate reportings for the salinity application from the bridge

--- src/apps/bridge/sensor_drivers/aanderaaConductivitySensor.cpp
+++ src/apps/bridge/sensor_drivers/aanderaaConductivitySensor.cpp
@@ -257,16 +257,10 @@ BmErr AanderaaConductivitySensor::send_spotter_log_aggregate(
 
   BmErr err = AbstractSensor::send_spotter_log_aggregate(
       "aanderaa_salinity", agg.reading_count,
-      "%.4f,"   // conductivity_mean_ms_cm
       "%.3f,"   // temperature_mean_deg_c
       "%.3f,"   // salinity_mean_psu
-      "%.4f,"   // salinity_std_dev
-      "%.3f,"   // water_density_mean_kg_m3
-      "%.3f,"   // sound_speed_mean_m_s
-      "%.3f\n", // depth_mean_m
-      agg.conductivity_mean_ms_cm, agg.temperature_mean_deg_c, agg.salinity_mean_psu,
-      agg.salinity_std_dev, agg.water_density_mean_kg_m3, agg.sound_speed_mean_m_s,
-      agg.depth_mean_m);
+      "%.4f\n", // salinity_std_dev
+      agg.temperature_mean_deg_c, agg.salinity_mean_psu, agg.salinity_std_dev);
 
   if (err != BmOK) {
     bm_debug("ERROR: Failed to send PME DO aggregate log to spotter, err: %d\n", err);
