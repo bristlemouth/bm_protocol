f19f50b519a7bdf0a6c43080dcfa5bba850bb9d1
Matthew Krause <kraus2mj@gmail.com>
2025-12-08 11:58:26 -0800

    Revert "Test/try reverting watchdog changes (#365)"
    This reverts commit 5d20f164707592bf00f3c173da6655495947dadf.

--- src/apps/rs232_expander_apps/rs232_expander_apps_common/app_main.cpp
+++ src/apps/rs232_expander_apps/rs232_expander_apps_common/app_main.cpp
@@ -268,9 +268,6 @@ static void defaultTask(void *parameters) {
   // platform boot before starting the watchdog task
   memfault_platform_boot();
 
-  #ifdef APP_AANDERAA_CURRENT_METER
-  startIWDGTask();
-  #endif // APP_AANDERAA_CURRENT_METER
   startSerial();
 
   startSerialConsole(&usbCLI);
--- src/lib/common/watchdog.c
+++ src/lib/common/watchdog.c
@@ -6,36 +6,8 @@
 #include "iwdg.h"
 #include "task_priorities.h"
 
-// If you want to use the memfault watchdog, uncomment the following include,
-// enable the LPTIM2 clock, and uncomment the memfault_software_watchdog_enable()
-// call in startIWDGTask() and the memfault_software_watchdog_feed() call in watchdogFeed()
-// #include "memfault/ports/watchdog.h"
-
 void watchdogFeed() {
   LL_IWDG_ReloadCounter(IWDG);
-  // memfault_software_watchdog_feed();
-}
-
-/***************************************************************************************
- *
- * Potential Watchdog issue relating to STM32u575 Errata 2.2.26
- *
- * We have observed that the Aanderaa Current Meter RS232 application has been
- * unexpectedly resetting during some of our testing. Through testing and git bisecting
- * we were able to determine that the commit that changed the watchdog from PR#316
- * was the commit that introduced the issue. Interestingly with the watchdog changes
- * this vApplicationIdleHook gets called before we enter Stop mode. Errata 2.2.26 states
- * that there are certain conditions that if met upon entering sleep would result in
- * the processor being stuck in stop mode with a system reset being the only recovery
- * method. So this errata, along with other theories are being investigated to root
- * cause the unexpected resets.
- *
- * While we are doing that we are going to go back to using the old watchdog for the
- * Current Meter Aanderaa RS232 application.
- *
-***************************************************************************************/
-void vApplicationIdleHook(void) {
-    watchdogFeed();
 }
 
 //
@@ -53,33 +25,10 @@ void vApplicationIdleHook(void) {
 //        periodically and the watchdog is only refreshed when several
 //        conditions are met.
 //
-#ifdef APP_AANDERAA_CURRENT_METER
-static void iWDGTask( void *parameters );
 
-void startIWDGTask() {
-  // memfault_software_watchdog_enable();
-  BaseType_t rval;
-  rval = xTaskCreate(
-              iWDGTask,
-              "IWDG",
-              configMINIMAL_STACK_SIZE,
-              NULL,
-              IWDG_TASK_PRIORITY,
-              NULL);
-
-  configASSERT(rval == pdTRUE);
-}
-
-static void iWDGTask( void *parameters ) {
-  // Don't warn about unused parameters
-  (void) parameters;
-
-  for(;;) {
+void vApplicationIdleHook(void) {
     watchdogFeed();
-    vTaskDelay(2 * 1000);
-  }
 }
-#endif // APP_AANDERAA_CURRENT_METER
 
 void IWDG_IRQHandler(void) {
   LL_IWDG_EnableWriteAccess(IWDG);
