36490b7a41b1ebd418a3c120a363cb30af9812cd
Victor Sowa <victor.sowa@sofarocean.com>
2026-01-13 14:23:52 -0800

    removed unused code and add in magnetic field print

--- src/apps/bm_devkit/bno085_imu_example/user_code/user_code.cpp
+++ src/apps/bm_devkit/bno085_imu_example/user_code/user_code.cpp
@@ -12,7 +12,6 @@
 #include "app_util.h"
 #include "bno085.h"
 
-
 extern "C" {
 #include "sh2.h"
 #include "sh2_util.h"
@@ -20,24 +19,7 @@ extern "C" {
 #include "sh2_SensorValue.h"
 }
 
-
-double gravity = 9.80665; // m/s^2
-double rho_salt = 1023.6; // kg/m^3
-double rho_fresh =  997.0474; // kg/m^3
-
-
-#define PRESSURE_IN_PASCALS(pressure) ((pressure * 100000))
-static constexpr char FRESH_WATER_FLAG[]  = "fresh_water";
-static constexpr uint8_t DEPTH_SENSOR_ADDR = 0x40;
-
-
-static uint32_t fresh_water = 0;
-
-
-
-
 static Bno085 bno085_imu(&i2c1, IMU_ADDR);
-
 
 // ISR callback for INT pin
 bool imuIntCallback(const void *pinHandle, uint8_t value, void *args) {
@@ -51,40 +33,6 @@ bool imuIntCallback(const void *pinHandle, uint8_t value, void *args) {
     }
     return true;
 }
-// // C-style wrapper functions for the HAL driver
-// static int bno085_open_wrapper(sh2_Hal_t* hal) {
-//     return bno085_imu.open(hal);
-// }
-
-
-// static void bno085_close_wrapper(sh2_Hal_t* hal) {
-//     bno085_imu.close(hal);
-// }
-
-
-// static int bno085_read_wrapper(sh2_Hal_t* hal, uint8_t* pBuffer, unsigned len, uint32_t* t_us) {
-//     return bno085_imu.read(hal, pBuffer, len, t_us);
-// }
-
-
-// static int bno085_write_wrapper(sh2_Hal_t* hal, uint8_t* pBuffer, unsigned len) {
-//     return bno085_imu.write(hal, pBuffer, len);
-// }
-
-
-// static uint32_t bno085_getTimeUs_wrapper(sh2_Hal_t* hal) {
-//     return bno085_imu.getTimeUs(hal);
-// }
-
-
-// sh2_Hal_t sh2_hal_driver = {
-//     .open = bno085_open_wrapper,
-//     .close = bno085_close_wrapper,
-//     .read = bno085_read_wrapper,
-//     .write = bno085_write_wrapper,
-//     .getTimeUs = bno085_getTimeUs_wrapper
-// };
-
 
 void event_callback(void *cookie, sh2_AsyncEvent_t *pEvent) {
   (void) cookie;
@@ -104,8 +52,6 @@ void event_callback(void *cookie, sh2_AsyncEvent_t *pEvent) {
   }
 }
 
-
-// __attribute((unused))
 static void printEvent(void * cookie, sh2_SensorEvent_t * event)
 {
     (void)cookie;
@@ -126,8 +72,25 @@ static void printEvent(void * cookie, sh2_SensorEvent_t * event)
 
     t = value.timestamp / 1000000.0;  // time in seconds.
     switch (value.sensorId) {
+        case SH2_MAGNETIC_FIELD_CALIBRATED:
+            printf("%8.4f Mag Field calibrated: x:%f y:%f z:%f \n",
+                  (double)t,
+                  value.un.magneticField.x,
+                  value.un.magneticField.y,
+                  value.un.magneticField.z);
+            break;
+
+        case SH2_MAGNETIC_FIELD_UNCALIBRATED:
+            printf("%8.4f Mag Field uncalibrated: x:%f y:%f z:%f \n",
+                  (double)t,
+                  value.un.magneticField.x,
+                  value.un.magneticField.y,
+                  value.un.magneticField.z);
+            break;
+
+
         case SH2_RAW_ACCELEROMETER:
-            printf("%8.4f Raw acc: %" PRIi16 " %" PRIi16 " %" PRIi16 " time_us:%" PRIu32 "\n",
+            printf("%8.4f Raw acc: x:%" PRIi16 " y:%" PRIi16 " z:%" PRIi16 " time_us:%" PRIu32 "\n",
                    (double)t,
                    value.un.rawAccelerometer.x,
                    value.un.rawAccelerometer.y,
@@ -137,7 +100,7 @@ static void printEvent(void * cookie, sh2_SensorEvent_t * event)
 
 
         case SH2_ACCELEROMETER:
-            printf("%8.4f Acc: %f %f %f\n",
+            printf("%8.4f Acc: x:%f y:%f z:%f\n",
                    (double)t,
                    (double)value.un.accelerometer.x,
                    (double)value.un.accelerometer.y,
@@ -243,111 +206,37 @@ static void printEvent(void * cookie, sh2_SensorEvent_t * event)
     }
 }
 
-
-// static PA7LD depth_sensor(&i2c1, DEPTH_SENSOR_ADDR);
-
-
 void setup(void) {
   /* USER ONE-TIME SETUP CODE GOES HERE */
   vTaskDelay(10000);
-  // depth_sensor.init();
-
 
   IORegisterCallback(&BF_IMU_INT, imuIntCallback, NULL);
 
-
-  get_config_uint(BM_CFG_PARTITION_SYSTEM, FRESH_WATER_FLAG, strlen(FRESH_WATER_FLAG), &fresh_water);
-
-
-  // int res = sh2_open(&sh2_hal_driver, event_callback, NULL);
-  // if (res != 0) {
-  //   printf("Failed to open sensor hub, res: %d\n", res);
-  // } else {
-  //   printf("Sensor hub opened successfully\n");
-  // }
-
-
   // // Enable sensor callback
-  //   sh2_setSensorCallback(printEvent, NULL);
   // Initialize BNO085 - this creates the service task
     if (!bno085_imu.init(event_callback, printEvent)) {
         printf("Failed to initialize BNO085\n");
         return;
     }
 
-
     vTaskDelay(1000);
 
-
-    // Configure sensors to report at specific intervals
-    // static sh2_SensorConfig_t config;
-
-
-    // // Enable rotation vector at 100Hz (10ms interval)
-    // config.changeSensitivityEnabled = false;
-    // config.wakeupEnabled = false;
-    // config.changeSensitivityRelative = false;
-    // config.alwaysOnEnabled = false;
-    // config.changeSensitivity = 0;
-    // config.reportInterval_us = 100000; // 100ms = 10Hz
-    // config.batchInterval_us = 0;
-
-
-    // res = sh2_setSensorConfig(SH2_ROTATION_VECTOR, &config);
-    // if (res != SH2_OK) {
-    //     printf("Failed to configure rotation vector: %d\n", res);
-    // }
-
-
-    // // Enable accelerometer at 100Hz
-    // res = sh2_setSensorConfig(SH2_ACCELEROMETER, &config);
-    // if (res != SH2_OK) {
-    //     printf("Failed to configure accelerometer: %d\n", res);
-    // }
-
-
-    // // Enable gyroscope at 100Hz
-    // res = sh2_setSensorConfig(SH2_GYROSCOPE_CALIBRATED, &config);
-    // if (res != SH2_OK) {
-    //     printf("Failed to configure gyroscope: %d\n", res);
-    // }
     // Configure sensors (10 Hz = 100ms interval)
-    bno085_imu.configureSensor(SH2_ROTATION_VECTOR, 100000);
-    bno085_imu.configureSensor(SH2_ACCELEROMETER, 100000);
-    bno085_imu.configureSensor(SH2_GYROSCOPE_CALIBRATED, 100000);
-
+    bno085_imu.configureSensor(SH2_ROTATION_VECTOR, 1000000);
+    bno085_imu.configureSensor(SH2_ACCELEROMETER, 1000000);
+    bno085_imu.configureSensor(SH2_GYROSCOPE_CALIBRATED, 1000000);
+    bno085_imu.configureSensor(SH2_MAGNETIC_FIELD_CALIBRATED, 1000000);
 
     printf("BNO085 configured and running\n");
-
-
 }
 
 
 void loop(void) {
   /* USER LOOP CODE GOES HERE */
-  // uint32_t curr_time_ms = bno085_imu.getTimeUs(&sh2_hal_driver)/1000;
   uint32_t curr_time_ms = uptimeGetMs();
   static uint32_t prev_time_ms = 0;
   if (curr_time_ms - 5000 > prev_time_ms) {
     printf("time: %" PRIu32 " ms\n", curr_time_ms);
     prev_time_ms = curr_time_ms;
-    // float pressure, temp;
-    // depth_sensor.readPTRaw(pressure, temp);
-    // double depth = 0.0;
-    // if (fresh_water) {
-    //   depth = PRESSURE_IN_PASCALS(pressure) / (gravity * rho_fresh);
-    // } else {
-    //   depth = PRESSURE_IN_PASCALS(pressure) / (gravity * rho_salt);
-    // }
-
-
-    // printf("pressure: %f bar, temp: %f °C, depth: %fm\n", pressure, temp, depth);
-
-
-    // TODO - publis the depth on the BM bus so we can use it to make decisions in other systems?
-    // Or we subscribe to other systems and tell them what to do to make sure we are correct here?
-    // sh2_service();
-
-
   }
 }
