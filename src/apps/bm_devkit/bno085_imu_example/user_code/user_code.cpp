cb6cf9ac25576aa085aeb87841c3305518415ab0
Victor Sowa <victor.sowa@sofarocean.com>
2026-01-13 11:25:37 -0800

    copied from my victor/depth_imu branch and modified to work with the devkit. looks like it is working!

---
+++ src/apps/bm_devkit/bno085_imu_example/user_code/user_code.cpp
@@ -0,0 +1,353 @@
+#include "user_code.h"
+#include "spotter.h"
+#include "pubsub.h"
+#include "bsp.h"
+#include "configuration.h"
+#include "debug.h"
+#include "lwip/inet.h"
+#include "sensors.h"
+#include "stm32_rtc.h"
+#include "task_priorities.h"
+#include "uptime.h"
+#include "app_util.h"
+#include "bno085.h"
+
+
+extern "C" {
+#include "sh2.h"
+#include "sh2_util.h"
+#include "sh2_err.h"
+#include "sh2_SensorValue.h"
+}
+
+
+double gravity = 9.80665; // m/s^2
+double rho_salt = 1023.6; // kg/m^3
+double rho_fresh =  997.0474; // kg/m^3
+
+
+#define PRESSURE_IN_PASCALS(pressure) ((pressure * 100000))
+static constexpr char FRESH_WATER_FLAG[]  = "fresh_water";
+static constexpr uint8_t DEPTH_SENSOR_ADDR = 0x40;
+
+
+static uint32_t fresh_water = 0;
+
+
+
+
+static Bno085 bno085_imu(&i2c1, IMU_ADDR);
+
+
+// ISR callback for INT pin
+bool imuIntCallback(const void *pinHandle, uint8_t value, void *args) {
+    (void)pinHandle;
+    (void)args;
+
+
+    // INT is active low, so trigger when it goes low (value == 0)
+    if (value == 0) {
+        bno085_imu.notifyDataReady();
+    }
+    return true;
+}
+// // C-style wrapper functions for the HAL driver
+// static int bno085_open_wrapper(sh2_Hal_t* hal) {
+//     return bno085_imu.open(hal);
+// }
+
+
+// static void bno085_close_wrapper(sh2_Hal_t* hal) {
+//     bno085_imu.close(hal);
+// }
+
+
+// static int bno085_read_wrapper(sh2_Hal_t* hal, uint8_t* pBuffer, unsigned len, uint32_t* t_us) {
+//     return bno085_imu.read(hal, pBuffer, len, t_us);
+// }
+
+
+// static int bno085_write_wrapper(sh2_Hal_t* hal, uint8_t* pBuffer, unsigned len) {
+//     return bno085_imu.write(hal, pBuffer, len);
+// }
+
+
+// static uint32_t bno085_getTimeUs_wrapper(sh2_Hal_t* hal) {
+//     return bno085_imu.getTimeUs(hal);
+// }
+
+
+// sh2_Hal_t sh2_hal_driver = {
+//     .open = bno085_open_wrapper,
+//     .close = bno085_close_wrapper,
+//     .read = bno085_read_wrapper,
+//     .write = bno085_write_wrapper,
+//     .getTimeUs = bno085_getTimeUs_wrapper
+// };
+
+
+void event_callback(void *cookie, sh2_AsyncEvent_t *pEvent) {
+  (void) cookie;
+  (void) pEvent;
+
+
+  // TODO - handle the event
+  // This is where we would handle the events from the sensor hub
+  // For now we will just print the event ID
+  printf("Received event with ID: %" PRIu32 "\n", pEvent->eventId);
+  if (pEvent->eventId == SH2_RESET) {
+    printf("EventHandler id: RESET\n");
+  } else if (pEvent->eventId == SH2_SHTP_EVENT) {
+    printf("EventHandler id: SHTP, %d\n", pEvent->shtpEvent);
+  } else if (pEvent->eventId == SH2_GET_FEATURE_RESP) {
+    printf("EventHandler id: Sensor config, %d\n", pEvent->sh2SensorConfigResp.sensorId);
+  }
+}
+
+
+// __attribute((unused))
+static void printEvent(void * cookie, sh2_SensorEvent_t * event)
+{
+    (void)cookie;
+    int rc;
+    sh2_SensorValue_t value;
+    float scaleRadToDeg = 180.0 / 3.14159265358;
+    float r, i, j, k, acc_deg, x, y, z;
+    float t;
+    static int skip = 0;
+
+
+    rc = sh2_decodeSensorEvent(&value, event);
+    if (rc != SH2_OK) {
+        printf("Error decoding sensor event: %d\n", rc);
+        return;
+    }
+
+
+    t = value.timestamp / 1000000.0;  // time in seconds.
+    switch (value.sensorId) {
+        case SH2_RAW_ACCELEROMETER:
+            printf("%8.4f Raw acc: %" PRIi16 " %" PRIi16 " %" PRIi16 " time_us:%" PRIu32 "\n",
+                   (double)t,
+                   value.un.rawAccelerometer.x,
+                   value.un.rawAccelerometer.y,
+                   value.un.rawAccelerometer.z,
+                   value.un.rawAccelerometer.timestamp);
+            break;
+
+
+        case SH2_ACCELEROMETER:
+            printf("%8.4f Acc: %f %f %f\n",
+                   (double)t,
+                   (double)value.un.accelerometer.x,
+                   (double)value.un.accelerometer.y,
+                   (double)value.un.accelerometer.z);
+            break;
+
+
+        case SH2_RAW_GYROSCOPE:
+            printf("%8.4f Raw gyro: x:%d y:%d z:%d temp:%d time_us:%" PRIu32 "\n",
+                   (double)t,
+                   value.un.rawGyroscope.x,
+                   value.un.rawGyroscope.y,
+                   value.un.rawGyroscope.z,
+                   value.un.rawGyroscope.temperature,
+                   value.un.rawGyroscope.timestamp);
+            break;
+
+
+        case SH2_ROTATION_VECTOR:
+            r = value.un.rotationVector.real;
+            i = value.un.rotationVector.i;
+            j = value.un.rotationVector.j;
+            k = value.un.rotationVector.k;
+            acc_deg = scaleRadToDeg *
+                value.un.rotationVector.accuracy;
+            printf("%8.4f Rotation Vector: "
+                   "r:%0.6f i:%0.6f j:%0.6f k:%0.6f (acc: %0.6f deg)\n",
+                   (double)t,
+                   (double)r, (double)i, (double)j, (double)k, (double)acc_deg);
+            break;
+        case SH2_GAME_ROTATION_VECTOR:
+            r = value.un.gameRotationVector.real;
+            i = value.un.gameRotationVector.i;
+            j = value.un.gameRotationVector.j;
+            k = value.un.gameRotationVector.k;
+            printf("%8.4f GRV: "
+                   "r:%0.6f i:%0.6f j:%0.6f k:%0.6f\n",
+                   (double)t,
+                   (double)r, (double)i, (double)j, (double)k);
+            break;
+        case SH2_GYROSCOPE_CALIBRATED:
+            x = value.un.gyroscope.x;
+            y = value.un.gyroscope.y;
+            z = value.un.gyroscope.z;
+            printf("%8.4f GYRO: "
+                   "x:%0.6f y:%0.6f z:%0.6f\n",
+                   (double)t,
+                   (double)x, (double)y, (double)z);
+            break;
+        case SH2_GYROSCOPE_UNCALIBRATED:
+            x = value.un.gyroscopeUncal.x;
+            y = value.un.gyroscopeUncal.y;
+            z = value.un.gyroscopeUncal.z;
+            printf("%8.4f GYRO_UNCAL: "
+                   "x:%0.6f y:%0.6f z:%0.6f\n",
+                   (double)t,
+                   (double)x, (double)y, (double)z);
+            break;
+        case SH2_GYRO_INTEGRATED_RV:
+            // These come at 1kHz, too fast to print all of them.
+            // So only print every 10th one
+            skip++;
+            if (skip == 10) {
+                skip = 0;
+                r = value.un.gyroIntegratedRV.real;
+                i = value.un.gyroIntegratedRV.i;
+                j = value.un.gyroIntegratedRV.j;
+                k = value.un.gyroIntegratedRV.k;
+                x = value.un.gyroIntegratedRV.angVelX;
+                y = value.un.gyroIntegratedRV.angVelY;
+                z = value.un.gyroIntegratedRV.angVelZ;
+                printf("%8.4f Gyro Integrated RV: "
+                       "r:%0.6f i:%0.6f j:%0.6f k:%0.6f x:%0.6f y:%0.6f z:%0.6f\n",
+                       (double)t,
+                       (double)r, (double)i, (double)j, (double)k,
+                       (double)x, (double)y, (double)z);
+            }
+            break;
+        case SH2_IZRO_MOTION_REQUEST:
+            printf("IZRO Request: intent:%d, request:%d\n",
+                   value.un.izroRequest.intent,
+                   value.un.izroRequest.request);
+            break;
+        case SH2_SHAKE_DETECTOR:
+            printf("Shake Axis: %c%c%c\n",
+                   (value.un.shakeDetector.shake & SHAKE_X) ? 'X' : '.',
+                   (value.un.shakeDetector.shake & SHAKE_Y) ? 'Y' : '.',
+                   (value.un.shakeDetector.shake & SHAKE_Z) ? 'Z' : '.');
+
+
+            break;
+        case SH2_STABILITY_CLASSIFIER:
+            printf("Stability Classification: %d\n",
+                   value.un.stabilityClassifier.classification);
+            break;
+        case SH2_STABILITY_DETECTOR:
+            printf("Stability Detector: %d\n",
+                   value.un.stabilityDetector.stability);
+            break;
+        default:
+            printf("Unknown sensor: %d\n", value.sensorId);
+            break;
+    }
+}
+
+
+// static PA7LD depth_sensor(&i2c1, DEPTH_SENSOR_ADDR);
+
+
+void setup(void) {
+  /* USER ONE-TIME SETUP CODE GOES HERE */
+  vTaskDelay(10000);
+  // depth_sensor.init();
+
+
+  IORegisterCallback(&BF_IMU_INT, imuIntCallback, NULL);
+
+
+  get_config_uint(BM_CFG_PARTITION_SYSTEM, FRESH_WATER_FLAG, strlen(FRESH_WATER_FLAG), &fresh_water);
+
+
+  // int res = sh2_open(&sh2_hal_driver, event_callback, NULL);
+  // if (res != 0) {
+  //   printf("Failed to open sensor hub, res: %d\n", res);
+  // } else {
+  //   printf("Sensor hub opened successfully\n");
+  // }
+
+
+  // // Enable sensor callback
+  //   sh2_setSensorCallback(printEvent, NULL);
+  // Initialize BNO085 - this creates the service task
+    if (!bno085_imu.init(event_callback, printEvent)) {
+        printf("Failed to initialize BNO085\n");
+        return;
+    }
+
+
+    vTaskDelay(1000);
+
+
+    // Configure sensors to report at specific intervals
+    // static sh2_SensorConfig_t config;
+
+
+    // // Enable rotation vector at 100Hz (10ms interval)
+    // config.changeSensitivityEnabled = false;
+    // config.wakeupEnabled = false;
+    // config.changeSensitivityRelative = false;
+    // config.alwaysOnEnabled = false;
+    // config.changeSensitivity = 0;
+    // config.reportInterval_us = 100000; // 100ms = 10Hz
+    // config.batchInterval_us = 0;
+
+
+    // res = sh2_setSensorConfig(SH2_ROTATION_VECTOR, &config);
+    // if (res != SH2_OK) {
+    //     printf("Failed to configure rotation vector: %d\n", res);
+    // }
+
+
+    // // Enable accelerometer at 100Hz
+    // res = sh2_setSensorConfig(SH2_ACCELEROMETER, &config);
+    // if (res != SH2_OK) {
+    //     printf("Failed to configure accelerometer: %d\n", res);
+    // }
+
+
+    // // Enable gyroscope at 100Hz
+    // res = sh2_setSensorConfig(SH2_GYROSCOPE_CALIBRATED, &config);
+    // if (res != SH2_OK) {
+    //     printf("Failed to configure gyroscope: %d\n", res);
+    // }
+    // Configure sensors (10 Hz = 100ms interval)
+    bno085_imu.configureSensor(SH2_ROTATION_VECTOR, 100000);
+    bno085_imu.configureSensor(SH2_ACCELEROMETER, 100000);
+    bno085_imu.configureSensor(SH2_GYROSCOPE_CALIBRATED, 100000);
+
+
+    printf("BNO085 configured and running\n");
+
+
+}
+
+
+void loop(void) {
+  /* USER LOOP CODE GOES HERE */
+  // uint32_t curr_time_ms = bno085_imu.getTimeUs(&sh2_hal_driver)/1000;
+  uint32_t curr_time_ms = uptimeGetMs();
+  static uint32_t prev_time_ms = 0;
+  if (curr_time_ms - 5000 > prev_time_ms) {
+    printf("time: %" PRIu32 " ms\n", curr_time_ms);
+    prev_time_ms = curr_time_ms;
+    // float pressure, temp;
+    // depth_sensor.readPTRaw(pressure, temp);
+    // double depth = 0.0;
+    // if (fresh_water) {
+    //   depth = PRESSURE_IN_PASCALS(pressure) / (gravity * rho_fresh);
+    // } else {
+    //   depth = PRESSURE_IN_PASCALS(pressure) / (gravity * rho_salt);
+    // }
+
+
+    // printf("pressure: %f bar, temp: %f °C, depth: %fm\n", pressure, temp, depth);
+
+
+    // TODO - publis the depth on the BM bus so we can use it to make decisions in other systems?
+    // Or we subscribe to other systems and tell them what to do to make sure we are correct here?
+    // sh2_service();
+
+
+  }
+}
---
+++ src/lib/drivers/bno085.cpp
@@ -0,0 +1,448 @@
+#include "bno085.h"
+#include "uptime.h"
+#include "debug.h"
+#include "bsp.h"
+#include "string.h"
+
+// Initialize static instance pointer
+Bno085* Bno085::_instance = nullptr;
+
+Bno085::Bno085(I2CInterface_t* i2cInterface, uint8_t address)
+    : _interface(i2cInterface),
+      _addr(address),
+      _serviceTaskHandle(NULL),
+      _i2cMutex(NULL),
+      _eventCallback(NULL),
+      _sensorCallback(NULL),
+      _initialized(false) {
+    _instance = this;
+}
+
+bool Bno085::init(sh2_EventCallback_t *eventCallback,
+                  sh2_SensorCallback_t *sensorCallback) {
+    _eventCallback = eventCallback;
+    _sensorCallback = sensorCallback;
+
+    // Create mutex for I2C access
+    _i2cMutex = xSemaphoreCreateMutex();
+    if (!_i2cMutex) {
+        return false;
+    }
+
+    // Create the service task
+    BaseType_t ret = xTaskCreate(
+        serviceTask,
+        "BNO085",
+        configMINIMAL_STACK_SIZE * 4,
+        this,
+        19,  // High priority to service sensor quickly
+        &_serviceTaskHandle
+    );
+
+    return ret == pdPASS;
+}
+
+void Bno085::notifyDataReady() {
+    // Called from ISR when INT pin goes low (data ready)
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
+    if (_serviceTaskHandle) {
+        vTaskNotifyGiveFromISR(_serviceTaskHandle, &xHigherPriorityTaskWoken);
+        portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+    }
+}
+
+void Bno085::serviceTask(void *arg) {
+    Bno085* instance = static_cast<Bno085*>(arg);
+
+    // Setup HAL
+    sh2_Hal_t hal;
+    hal.open = Bno085::open;
+    hal.close = Bno085::close;
+    hal.read = Bno085::read;
+    hal.write = Bno085::write;
+    hal.getTimeUs = Bno085::getTimeUs;
+
+    // Open sensor hub
+    int status = sh2_open(&hal, instance->_eventCallback, NULL);
+    if (status != SH2_OK) {
+        printf("BNO085: Failed to open sensor hub: %d\n", status);
+        vTaskDelete(NULL);
+        return;
+    }
+
+    printf("BNO085: Sensor hub opened successfully\n");
+
+    // Set sensor callback
+    if (instance->_sensorCallback) {
+        sh2_setSensorCallback(instance->_sensorCallback, NULL);
+    }
+
+    instance->_initialized = true;
+
+     // Main service loop - wait for INT pin notification
+    while (1) {
+      // Wait for notification from ISR (INT pin asserted)
+      // Timeout after 100ms to periodically check even if no interrupt
+      uint32_t notificationValue = ulTaskNotifyTake(pdTRUE, 10);
+
+      if (notificationValue > 0) {
+        // INT pin was asserted (went LOW), data is ready
+            // Keep servicing until INT goes back HIGH
+            uint8_t intPinState;
+            do {
+                sh2_service();
+
+                // Check if INT pin is still LOW (more data available)
+                IORead(&BF_IMU_INT, &intPinState);
+
+            } while (intPinState == 0);  // Continue while INT is LOW
+      } else {
+        // Timeout - call sh2_service() anyway for housekeeping
+        // This handles cases where we might have missed an interrupt
+        sh2_service();
+      }
+    }
+}
+
+bool Bno085::configureSensor(sh2_SensorId_t sensorId, uint32_t reportInterval_us) {
+    if (!_initialized) {
+        return false;
+    }
+
+    sh2_SensorConfig_t config;
+    config.changeSensitivityEnabled = false;
+    config.wakeupEnabled = false;
+    config.changeSensitivityRelative = false;
+    config.alwaysOnEnabled = false;
+    config.changeSensitivity = 0;
+    config.reportInterval_us = reportInterval_us;
+    config.batchInterval_us = 0;
+
+    int status = sh2_setSensorConfig(sensorId, &config);
+    if (status != SH2_OK) {
+        printf("BNO085: Failed to configure sensor %d: %d\n", sensorId, status);
+        return false;
+    }
+
+    return true;
+}
+
+int Bno085::open(sh2_Hal_t *self) {
+    (void)self;
+    // Nothing special needed for I2C
+    return SH2_OK;
+}
+
+void Bno085::close(sh2_Hal_t *self) {
+    (void)self;
+    // Nothing special needed
+}
+
+int Bno085::read(sh2_Hal_t *self, uint8_t *pBuffer, unsigned len, uint32_t *t_us) {
+    (void)self;
+
+    // Use the static instance pointer
+    if (!_instance) {
+        return 0;
+    }
+
+    return _instance->readBytes(pBuffer, len, t_us);
+}
+
+int Bno085::write(sh2_Hal_t *self, uint8_t *pBuffer, unsigned len) {
+    (void)self;
+
+    // Use the static instance pointer
+    if (!_instance) {
+        return 0;
+    }
+
+    return _instance->writeBytes(pBuffer, len);
+}
+
+uint32_t Bno085::getTimeUs(sh2_Hal_t *self) {
+    (void)self;
+    return uptimeGetMs() * 1000;
+}
+
+// Instance methods that do the actual work
+int Bno085::readBytes(uint8_t *pBuffer, unsigned len, uint32_t *t_us) {
+    if (xSemaphoreTake(_i2cMutex, pdMS_TO_TICKS(100)) != pdTRUE) {
+        return 0;
+    }
+
+    int bytesRead = 0;
+
+    // // Read SHTP header (4 bytes)
+    // uint8_t header[4];
+    // if (i2cTxRx(_interface, _addr, NULL, 0, header, 4, 100) == 0) {
+
+    //     // Parse cargo length from header
+    //     uint16_t cargoLength = (header[0] | (header[1] << 8)) & 0x7FFF;
+
+    //     if (cargoLength > 0 && cargoLength <= len) {
+    //         // Copy header to output buffer
+    //         memcpy(pBuffer, header, 4);
+
+    //         // Read remaining cargo if any
+    //         if (cargoLength > 4) {
+    //             if (i2cTxRx(_interface, _addr, NULL, 0,
+    //                        pBuffer + 4, cargoLength - 4, 100) == 0) {
+    //                 bytesRead = cargoLength;
+    //             }
+    //         } else {
+    //             bytesRead = 4;
+    //         }
+
+    //         // Set timestamp
+    //         if (t_us) {
+    //             *t_us = uptimeGetMs() * 1000;
+    //         }
+    //     }
+    // }
+
+    // Read maximum SHTP packet size in ONE I2C transaction
+    // The BNO085 will provide the actual length in the header
+    // SH2_HAL_MAX_TRANSFER_IN is typically 512 bytes
+    const uint16_t maxReadSize = (len < SH2_HAL_MAX_TRANSFER_IN) ? len : SH2_HAL_MAX_TRANSFER_IN;
+
+    // Single I2C read transaction - this is key!
+    if (i2cTxRx(_interface, _addr, NULL, 0, pBuffer, maxReadSize, 100) == 0) {
+
+        // Now parse the header from what we just read
+        uint16_t cargoLength = (pBuffer[0] | (pBuffer[1] << 8)) & 0x7FFF;
+
+        if (cargoLength > 0 && cargoLength <= maxReadSize) {
+            // We got valid data
+            bytesRead = cargoLength;
+
+            // Set timestamp
+            if (t_us) {
+                *t_us = uptimeGetMs() * 1000;
+            }
+        }
+        // If cargoLength is 0, no data was available - return 0
+    }
+
+
+    xSemaphoreGive(_i2cMutex);
+    return bytesRead;
+}
+
+int Bno085::writeBytes(uint8_t *pBuffer, unsigned len) {
+    if (xSemaphoreTake(_i2cMutex, pdMS_TO_TICKS(100)) != pdTRUE) {
+        return 0;
+    }
+
+    int bytesWritten = 0;
+    if (i2cTxRx(_interface, _addr, pBuffer, len, NULL, 0, 100) == 0) {
+        bytesWritten = len;
+    }
+
+    xSemaphoreGive(_i2cMutex);
+    return bytesWritten;
+}
+
+// Bno085::Bno085(I2CInterface_t* i2cInterface, uint8_t address) {
+//   _interface = i2cInterface;
+//   _addr = static_cast<uint8_t>(address);
+// }
+
+// bool Bno085::init() {
+//   return true;
+// }
+
+// int Bno085::open(sh2_Hal_t *self) {
+//   (void) self;
+//   // TODO - this is what needs to happen here:
+
+//   // This function initializes communications with the device.  It
+//   // can initialize any GPIO pins and peripheral devices used to
+//   // interface with the sensor hub.
+//   // It should also perform a reset cycle on the sensor hub to
+//   // ensure communications start from a known state.
+
+//   // My notes:
+//   // I believe since we always have the bus on we just need to
+//   // do the reset portion of above
+
+//   // I'm not sure what needs to be returned here for success/fail
+//   return SH2_OK;
+// }
+
+// void Bno085::close(sh2_Hal_t *self) {
+//   (void) self;
+//   // TODO:
+//   // This function completes communications with the sensor hub.
+//   // It should put the device in reset then de-initialize any
+//   // peripherals or hardware resources that were used.
+
+//   // My notes:
+//   // Since we are not actually turning off the HAL/I2C bus here
+//   // I think maybe we just use a private variable to block tx/rx
+//   // from the sensor
+// }
+
+// int Bno085::read(sh2_Hal_t *self, uint8_t *pBuffer, unsigned len, uint32_t *t_us) {
+//   (void) self;
+//   (void)t_us;
+
+//   Bno085* instance = static_cast<Bno085*>(self->user);
+
+//     if (xSemaphoreTake(instance->_i2cMutex, pdMS_TO_TICKS(100)) != pdTRUE) {
+//         return 0;
+//     }
+
+//     int bytesRead = 0;
+
+//     // Read SHTP header (4 bytes)
+//     uint8_t header[4];
+//     if (i2cTxRx(instance->_interface, instance->_addr, NULL, 0,
+//                 header, 4, 100) == 0) {
+
+//         // Parse cargo length from header
+//         uint16_t cargoLength = (header[0] | (header[1] << 8)) & 0x7FFF;
+
+//         if (cargoLength > 0 && cargoLength <= len) {
+//             // Copy header to output buffer
+//             memcpy(pBuffer, header, 4);
+
+//             // Read remaining cargo if any
+//             if (cargoLength > 4) {
+//                 if (i2cTxRx(instance->_interface, instance->_addr, NULL, 0,
+//                            pBuffer + 4, cargoLength - 4, 100) == 0) {
+//                     bytesRead = cargoLength;
+//                 }
+//             } else {
+//                 bytesRead = 4;
+//             }
+
+//             // Set timestamp
+//             if (t_us) {
+//                 *t_us = uptimeGetMs() * 1000;
+//             }
+//         }
+//     }
+
+//     xSemaphoreGive(instance->_i2cMutex);
+//     return bytesRead;
+
+//   // TODO:
+
+//   // This function supports reading data from the sensor hub.
+//   // It will be called frequently to service the device.
+//   //
+//   // If the HAL has received a full SHTP transfer, this function
+//   // should load the data into pBuffer, set the timestamp to the
+//   // time the interrupt was detected, and return the non-zero length
+//   // of data in this transfer.
+//   //
+//   // If the HAL has not recevied a full SHTP transfer, this function
+//   // should return 0.
+//   //
+//   // Because this function is called regularly, it can be used to
+//   // perform other housekeeping operations.  (In the case of UART
+//   // interfacing, bytes transmitted are staggered in time and this
+//   // function can be used to keep the transmission flowing.)
+
+
+//   // My notes:
+//   // One thing I need to find out is how the interrupt line
+//   // plays into recieving... if we even need it?
+//   // TODO is to get this to return the number of bytes it read...
+//     // readBytes(pBuffer, len);
+//     // printf("xfer size: %d\n", i2c1.handle->XferSize);
+//     // // static bool is_first_read = true;
+//     // // if (is_first_read) {
+//     // //   is_first_read = false;
+//     // //   return 4;
+//     // // }
+
+//     // return 255 - (i2c1.handle->XferSize);
+//     // return 2;
+//   // }
+
+
+//   // return 0;
+
+//     // For blocking I2C, we need to check if data is available first
+//     // The BNO085 uses a header byte to indicate data length
+//     // uint8_t header[4];
+
+//     // // Read the 4-byte SHTP header (blocking)
+//     // if (HAL_I2C_Master_Receive(i2c1.handle, I2C_ADDR << 1, header, 4, 100) != HAL_OK) {
+//     //     return 0; // No data available or error
+//     // }
+
+//     // // Parse SHTP header to get cargo length
+//     // uint16_t cargoLength = ((uint16_t)header[0] + ((uint16_t)header[1] << 8)) & ~0x8000;
+
+//     // if (cargoLength == 0) {
+//     //     return 0; // No data
+//     // }
+
+//     // if (cargoLength > len) {
+//     //     return 0; // Buffer too small
+//     // }
+
+//     // // Copy header to buffer
+//     // memcpy(pBuffer, header, 4);
+
+//     // // Read remaining cargo if any (blocking)
+//     // if (cargoLength > 4) {
+//     //     if (HAL_I2C_Master_Receive(i2c1.handle, I2C_ADDR << 1,
+//     //                                pBuffer + 4, cargoLength - 4, 100) != HAL_OK) {
+//     //         return 0;
+//     //     }
+//     // }
+
+//     // // Get timestamp
+//     // if (t_us) {
+//     //     *t_us = getTimeUs(self);
+//     // }
+//     // printf("cargoLength: %d\n", cargoLength);
+//     // // printf("cargo: ");
+//     // // for (int j = 0; j < cargoLength; j++) {
+//     // //   printf("0x%02" PRIx8 ",", pBuffer[j]);
+//     // // }
+//     // // printf("\n");
+//     // return cargoLength;
+
+// }
+
+// int Bno085::write(sh2_Hal_t *self, uint8_t *pBuffer, unsigned len) {
+//   (void) self;
+
+//   // TODO:
+//   // This function supports writing data to the sensor hub.
+//   // It is called each time the application has a block of data to
+//   // transfer to the device.
+//   //
+//   // If the device isn't ready to receive data, this function can
+//   // return 0 without performing the transmit function.
+//   //
+//   // If the transmission can be started, this function needs to
+//   // copy the data from pBuffer and return the number of bytes
+//   // accepted.  It need not block.  The actual transmission of
+//   // the data can continue after this function returns.
+
+//   // return writeBytes(pBuffer, len);
+//   // Blocking I2C write
+//   if (HAL_I2C_Master_Transmit(i2c1.handle, I2C_ADDR << 1, pBuffer, len, 100) != HAL_OK) {
+//       return 0;
+//   }
+
+//   return len;
+// }
+
+// uint32_t Bno085::getTimeUs(sh2_Hal_t *self) {
+//   (void)self;
+//   // This function should return a 32-bit value representing a
+//   // microsecond counter.  The count may roll over after 2^32
+//   // microseconds.
+
+//   // My notes:
+//   // Done!
+//   return (uptimeGetMs() * 1000);
+// }
